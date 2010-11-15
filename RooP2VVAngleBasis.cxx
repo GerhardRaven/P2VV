b843b00521dfa75467a77fb1f8ff4810f8c62d4a
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2010-11-15 16:59:55 +0100

    implement RooP2VVAngleBasis::createProduct

--- RooP2VVAngleBasis.cxx
+++ RooP2VVAngleBasis.cxx
@@ -36,12 +36,35 @@ RooP2VVAngleBasis::RooP2VVAngleBasis()
 }
 
 //_____________________________________________________________________________
-// require RooRealVar observables, so that we know they are independent...
 RooP2VVAngleBasis::RooP2VVAngleBasis( const char *name, const char *title
                                     , RooAbsReal& cpsi, RooAbsReal& ctheta, RooAbsReal& phi
                                     , int i, int j, int l, int m, double c )
  : RooProduct(name, title,RooArgSet())
+ , _c(c)
  , _i(i), _j(j), _l(l), _m(m)
+ , _prod(false)
+{
+  std::stringstream P,Y;
+  P << name << "_P_" << i << ( j<0 ? "_m" : "_" )  << (j<0?-j:j) ;
+  _compRSet.addOwned(*new RooLegendre(   P.str().c_str(), P.str().c_str(), cpsi,i,j) );
+  Y << name << "_Y_" << l << ( m<0 ? "_m" : "_" )  << (m<0?-m:m) ;
+  _compRSet.addOwned(*new RooSpHarmonic( Y.str().c_str(), Y.str().c_str(), ctheta,phi,l,m) );
+  if (c!=1) {
+    std::stringstream C;
+    C << name << ( c<0 ? "_m" : "_" ) << ( c<0?-c:c ) ;
+    _compRSet.addOwned(*new RooConstVar( C.str().c_str(), C.str().c_str(), c ) );
+  }
+}
+
+RooP2VVAngleBasis::RooP2VVAngleBasis( const char *name, const char *title
+                                    , RooAbsReal& cpsi, RooAbsReal& ctheta, RooAbsReal& phi
+                                    , int i1, int j1, int l1, int m1
+                                    , int i2, int j2, int l2, int m2
+                                    , double c )
+ : RooProduct(name, title,RooArgSet())
+ , _c(c)
+ , _i(i1), _j(j1), _l(l1), _m(m1)
+ , _prod(true)
 {
   if (c!=1) {
     std::stringstream C;
@@ -49,16 +72,54 @@ RooP2VVAngleBasis::RooP2VVAngleBasis( const char *name, const char *title
     _compRSet.addOwned(*new RooConstVar( C.str().c_str(), C.str().c_str(), c ) );
   }
   std::stringstream P,Y;
-  P << name << "_P_" << i << ( j<0 ? "_m" : "_" )  << (j<0?-j:j) ;
-  _compRSet.addOwned(*new RooLegendre(   P.str().c_str(), P.str().c_str(), cpsi,i,j) );
-  Y << name << "_Y_" << l << ( m<0 ? "_m" : "_" )  << (m<0?-m:m) ;
-  _compRSet.addOwned(*new RooSpHarmonic( Y.str().c_str(), Y.str().c_str(), ctheta,phi,l,m) );
+  P << name << "_P_" << i1 << ( j1<0 ? "_m" : "_" )  << (j1<0?-j1:j1) 
+            <<  "_"  << i2 << ( j2<0 ? "_m" : "_" )  << (j2<0?-j2:j2) ;
+  _compRSet.addOwned(*new RooLegendre(   P.str().c_str(), P.str().c_str(), cpsi,i1,j1, i2, j2) );
+  Y << name << "_Y_" << l1 << ( m1<0 ? "_m" : "_" )  << (m1<0?-m1:m1) 
+            <<  "_"  << l2 << ( m2<0 ? "_m" : "_" )  << (m2<0?-m2:m2) ;
+  _compRSet.addOwned(*new RooSpHarmonic( Y.str().c_str(), Y.str().c_str(), ctheta,phi,l1,m1,l2,m2) );
 }
-
 //_____________________________________________________________________________
 RooP2VVAngleBasis::RooP2VVAngleBasis(const RooP2VVAngleBasis& other, const char* name) 
     : RooProduct(other, name)
+    , _c(other._c)
     , _i(other._i), _j(other._j), _l(other._l), _m(other._m)
+    , _prod(other._prod)
 {
 }
 
+RooP2VVAngleBasis* 
+RooP2VVAngleBasis::createProduct(int i, int l, int m, double c) const {
+      std::stringstream name; name << this->GetName() << "_x_" << i << "_" << l << ( m<0 ? "_m":"_" ) << (m<0?-m:m) 
+                                                               << ( c<0 ? "_m" : "_" ) << ( c<0?-c:c ) ;
+      // grab first function, dynamic_cast to RooLegendre, grab its observable...
+      _compRIter->Reset();
+      RooLegendre *P = dynamic_cast<RooLegendre*>(_compRIter->Next());
+      RooSpHarmonic *Y = dynamic_cast<RooSpHarmonic*>(_compRIter->Next());
+      assert(P!=0);
+      assert(Y!=0);
+      RooArgSet* Po = P->getParameters((RooAbsData*) 0);
+      RooArgSet* Yo = Y->getParameters((RooAbsData*) 0);
+      assert(Po->getSize()==1);  
+      assert(Yo->getSize()==2);  
+      TIterator* iter = Po->createIterator();
+      RooAbsReal *cpsi = dynamic_cast<RooAbsReal*>(iter->Next());
+      assert(cpsi!=0);
+      //cpsi->Print("V");
+      delete iter;
+      iter = Yo->createIterator();
+      RooAbsReal *ctheta = dynamic_cast<RooAbsReal*>(iter->Next());
+      assert(ctheta!=0);
+      //ctheta->Print("V");
+      RooAbsReal *phi = dynamic_cast<RooAbsReal*>(iter->Next());
+      assert(phi!=0);
+      //phi->Print("V");
+      delete iter;
+
+
+      return (!_prod) ? new  RooP2VVAngleBasis( name.str().c_str(), name.str().c_str()
+                                              , *cpsi, *ctheta, *phi
+                                              , _i, _j, _l, _m
+                                              ,  i,  0,  l,  m
+                                              , _c * c ) : 0 ;
+  }
--- RooP2VVAngleBasis.h
+++ RooP2VVAngleBasis.h
@@ -20,19 +20,23 @@
 class RooP2VVAngleBasis : public RooProduct {
 public:
   RooP2VVAngleBasis() ;
-  RooP2VVAngleBasis(const char *name, const char *title, RooAbsReal& cpsi, RooAbsReal& ctheta, RooAbsReal& phi, int i, int j,int l, int m, double c = 1. );
+  RooP2VVAngleBasis(const char *name, const char *title, RooAbsReal& cpsi, RooAbsReal& ctheta, RooAbsReal& phi, int i, int j, int l, int m, double c = 1. );
+  RooP2VVAngleBasis(const char *name, const char *title, RooAbsReal& cpsi, RooAbsReal& ctheta, RooAbsReal& phi, int i1, int j1, int l1, int m1
+                                                                                                              , int i2, int j2, int l1, int m2
+                                                                                                              , double c = 1. );
 
   RooP2VVAngleBasis(const RooP2VVAngleBasis& other, const char* name = 0);
   virtual TObject* clone(const char* newname) const { return new RooP2VVAngleBasis(*this, newname); }
   inline virtual ~RooP2VVAngleBasis() { }
 
   // create a new RooAbsReal which is 'us' multiplied by an efficiency factor
-  //RooP2VVAngleBasis* createProduct(int i, int j, int k, double eps_ijk) {
-        //return 0; //TODO: implement
-  //}
+  // Note: we can only multiply once... 
+  RooP2VVAngleBasis* createProduct(int i, int l, int m, double eps_ilm) const;
 
 private: 
+  double _c;
   int _i,_j,_l,_m;
+  bool _prod;
 
   ClassDef(RooP2VVAngleBasis,1) // 
 };
