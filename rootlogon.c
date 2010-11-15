39fd73f9dcfce61b0077c32d38e6e578a1b5b3f2
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2010-11-15 15:35:31 +0100

    load AngleBasis in rootlogon

--- rootlogon.c
+++ rootlogon.c
@@ -3,4 +3,5 @@ void rootlogon() {
     gSystem->Load("libMathMore.so");
     gROOT->ProcessLine(".L RooLegendre.cxx+");
     gROOT->ProcessLine(".L RooSpHarmonic.cxx+");
+    gROOT->ProcessLine(".L RooP2VVAngleBasis.cxx+");
 }
