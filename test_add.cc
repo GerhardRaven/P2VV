ff9471af1e09453e25cf738dbe816a7238648a72
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2010-11-18 13:44:26 +0100

    fast test_add!

--- test_add.cc
+++ test_add.cc
@@ -17,13 +17,8 @@
 #include "TCanvas.h"
 #endif
 using namespace std;
-/*
-gSystem->Load("libMathMore.so")
-.L RooLegendre.cxx+g
-.L RooSpHarmonic.cxx+g
-*/
 
-RooAbsReal& prod(RooWorkspace& w,RooAbsReal* a, RooAbsReal* b, RooAbsReal* c, RooAbsReal* d) {
+RooAbsReal& prod(RooWorkspace& w,RooAbsReal* a, RooAbsReal* b, RooAbsReal* c, RooAbsReal* d, RooAbsReal* e) {
     assert (a!=0) ;
     if (b==0&&c==0&&d==0) return *a;
     const char *name(0);
@@ -31,14 +26,16 @@ RooAbsReal& prod(RooWorkspace& w,RooAbsReal* a, RooAbsReal* b, RooAbsReal* c, Ro
     if (b) { name = Format("%s_x_%s",name,b->GetName()); l.add(*b); }
     if (c) { name = Format("%s_x_%s",name,c->GetName()); l.add(*c); }
     if (d) { name = Format("%s_x_%s",name,d->GetName()); l.add(*d); }
+    if (e) { name = Format("%s_x_%s",name,e->GetName()); l.add(*e); }
     return import(w,RooProduct(name,name,l));
 }
 
-RooAbsReal& prod(RooWorkspace& w,const char* a, const char *b=0, const char *c=0, const char *d=0) {
+RooAbsReal& prod(RooWorkspace& w,const char* a, const char *b=0, const char *c=0, const char *d=0, const char *e=0) {
     return prod( w,      &get<RooAbsReal>(w,a)
                   ,  b ? &get<RooAbsReal>(w,b) : 0
                   ,  c ? &get<RooAbsReal>(w,c) : 0
-                  ,  d ? &get<RooAbsReal>(w,d) : 0 );
+                  ,  d ? &get<RooAbsReal>(w,d) : 0
+                  ,  e ? &get<RooAbsReal>(w,e) : 0 );
 }
 
 RooAbsPdf& jpsiphi(RooWorkspace& w, const char* name 
@@ -51,51 +48,50 @@ RooAbsPdf& jpsiphi(RooWorkspace& w, const char* name
 { 
         // definition of the angular part of the PDF in terms of basis functions... 
         abasis ab(w, "cpsi", "ctheta", "phi");  // bind workspace and observables
-        import(w, RooAddition_("AzAz_basis","AzAz_basis",RooArgList( ab("AzAz",       0,0,0, 0, 2.), ab("AzAz",       0,0,2,0, sqrt(1./ 5.)),  ab("AzAz",     0,0,2,2, -sqrt( 3./ 5.)) , 
-                                                                     ab("AzAz",       2,0,0, 0, 4.), ab("AzAz",       2,0,2,0, sqrt(4./ 5.)),  ab("AzAz",     2,0,2,2, -sqrt(12./ 5.)) )));
-        import(w, RooAddition_("AparApar_basis","AparApar_basis",RooArgList( ab("AparApar",   2,2,0, 0, 1.), ab("AparApar",   2,2,2,0, sqrt(1./20.)),  ab("AparApar", 2,2,2,2,  sqrt( 3./20.)) ))); 
-        import(w, RooAddition_("AperpAperp_basis","AperpAperp_basis",RooArgList( ab("AperpAperp", 2,2,0, 0, 1.), ab("AperpAperp", 2,2,2,0,-sqrt(1./ 5.)))));
-        import(w, RooAddition_("AparAperp_basis","AparAperp_basis",RooArgList( ab("AparAperp",  2,2,2,-1, sqrt( 9./15.)) )));
-        import(w, RooAddition_("AzAperp_basis","AzAperp_basis",RooArgList( ab("AzAperp",    2,1,2, 1,-sqrt(18./15.)) )));
-        import(w, RooAddition_("AzApar_basis","AzApar_basis",RooArgList( ab("AzApar",     2,1,2,-2, sqrt(18./15.)) )));
-
-        //                        0    1    2       3       4      5      6    7 8          9                10             11              12            13
-        RooArgSet amp = w.argSet("ReAz,ImAz,ReAperp,ImAperp,ReApar,ImApar,qtag,C,AzAz_basis,AperpAperp_basis,AparApar_basis,AparAperp_basis,AzAperp_basis,AzApar_basis");
-        import(w,RooFormulaVar("NAzAz",       "@8 * ( @0 * @0 + @1 * @1 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("NAparApar",   "@10* ( @4 * @4 + @5 * @5 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("NAperpAperp", "@9 * ( @2 * @2 + @3 * @3 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("ReAparAperp", "@11* ( @4 * @2 + @5 * @3 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("ReAzAperp",   "@12* ( @0 * @2 + @1 * @3 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("ReAzApar",    "@13* ( @0 * @4 + @1 * @5 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("ImAparAperp", "@11* ( @4 * @3 - @5 * @2 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("ImAzAperp",   "@12* ( @0 * @3 - @1 * @2 ) / ( 1+@6*@7 )", amp));
-        import(w,RooFormulaVar("qtag_",   "@7", amp));
+        import(w, RooAddition_("AzAz_basis",      "AzAz_basis",       RooArgList( ab("AzAz",       0,0,0, 0, 2.), ab("AzAz",       0,0,2,0, sqrt(1./ 5.)),  ab("AzAz",     0,0,2,2, -sqrt( 3./ 5.)) , 
+                                                                                  ab("AzAz",       2,0,0, 0, 4.), ab("AzAz",       2,0,2,0, sqrt(4./ 5.)),  ab("AzAz",     2,0,2,2, -sqrt(12./ 5.)) )));
+        import(w, RooAddition_("AparApar_basis",  "AparApar_basis",   RooArgList( ab("AparApar",   2,2,0, 0, 1.), ab("AparApar",   2,2,2,0, sqrt(1./20.)),  ab("AparApar", 2,2,2,2,  sqrt( 3./20.)) ))); 
+        import(w, RooAddition_("AperpAperp_basis","AperpAperp_basis", RooArgList( ab("AperpAperp", 2,2,0, 0, 1.), ab("AperpAperp", 2,2,2,0,-sqrt(1./ 5.)))));
+        import(w, RooAddition_("AparAperp_basis", "AparAperp_basis",  RooArgList( ab("AparAperp",  2,2,2,-1, sqrt( 9./15.)) )));
+        import(w, RooAddition_("AzAperp_basis",   "AzAperp_basis",    RooArgList( ab("AzAperp",    2,1,2, 1,-sqrt(18./15.)) )));
+        import(w, RooAddition_("AzApar_basis",    "AzApar_basis",     RooArgList( ab("AzApar",     2,1,2,-2, sqrt(18./15.)) )));
+        //                        0    1    2       3       4      5      6    7
+        RooArgSet amp = w.argSet("ReAz,ImAz,ReAperp,ImAperp,ReApar,ImApar,qtag,C");
+        import(w,RooFormulaVar("NAzAz",       "( @0 * @0 + @1 * @1 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("NAparApar",   "( @4 * @4 + @5 * @5 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("NAperpAperp", "( @2 * @2 + @3 * @3 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("ReAparAperp", "( @4 * @2 + @5 * @3 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("ReAzAperp",   "( @0 * @2 + @1 * @3 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("ReAzApar",    "( @0 * @4 + @1 * @5 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("ImAparAperp", "( @4 * @3 - @5 * @2 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("ImAzAperp",   "( @0 * @3 - @1 * @2 ) / ( 1+@6*@7 )", amp));
+        import(w,RooFormulaVar("qtag_",   "@7", amp)); // make a RooAbsReal corresponding to the RooCategory 'qtag'...
 
         w.factory("Minus[-1]");
-        import(w, RooAddition_("f_cosh","f_cosh",RooArgSet( prod(w, "NAzAz"                 )
-                                                          , prod(w, "NAparApar"             )
-                                                          , prod(w, "NAperpAperp"           )
-                                                          , prod(w, "ImAparAperp","qtag_","C")
-                                                          , prod(w, "ImAzAperp",  "qtag_","C")
-                                                          , prod(w, "ReAzApar"              ) )));
-        import(w, RooAddition_("f_cos" ,"f_cos", RooArgSet( prod(w, "NAzAz",    "qtag_","C")
-                                                          , prod(w, "NAparApar",  "qtag_","C")
-                                                          , prod(w, "NAperpAperp","qtag_","C")
-                                                          , prod(w, "ImAparAperp"           )
-                                                          , prod(w, "ImAzAperp"             )
-                                                          , prod(w, "ReAzApar",   "qtag_","C") )));
-        import(w, RooAddition_("f_sinh","f_sinh",RooArgSet( prod(w, "NAzAz",      "Minus",       "D")
-                                                          , prod(w, "NAparApar",  "Minus",       "D")
-                                                          , prod(w, "NAperpAperp",               "D")
-                                                          , prod(w, "ReAparAperp",        "qtag_","S")
-                                                          , prod(w, "ReAzAperp",          "qtag_","S")
-                                                          , prod(w, "ReAzApar",   "Minus",       "D") )));
-        import(w, RooAddition_("f_sin" ,"f_sin", RooArgSet( prod(w, "NAzAz",       "Minus","qtag_","S")
-                                                          , prod(w, "NAparApar",   "Minus","qtag_","S")
-                                                          , prod(w, "NAperpAperp",         "qtag_","S")
-                                                          , prod(w, "ReAparAperp", "Minus",       "D")
-                                                          , prod(w, "ReAzAperp",   "Minus",       "D")
-                                                          , prod(w, "ReAzApar",    "Minus","qtag_","S") )));
+        import(w, RooAddition_("f_cosh","f_cosh",RooArgSet( prod(w, "NAzAz",                           "AzAz_basis"      )
+                                                          , prod(w, "NAparApar",                       "AparApar_basis"  )
+                                                          , prod(w, "NAperpAperp",                     "AperpAperp_basis")
+                                                          , prod(w, "ImAparAperp",         "qtag_","C","AparAperp_basis")
+                                                          , prod(w, "ImAzAperp",           "qtag_","C","AzAperp_basis")
+                                                          , prod(w, "ReAzApar",                        "AzApar_basis") )));
+        import(w, RooAddition_("f_cos" ,"f_cos", RooArgSet( prod(w, "NAzAz",               "qtag_","C","AzAz_basis")
+                                                          , prod(w, "NAparApar",           "qtag_","C","AparApar_basis")
+                                                          , prod(w, "NAperpAperp",         "qtag_","C","AperpAperp_basis")
+                                                          , prod(w, "ImAparAperp",                     "AparAperp_basis")
+                                                          , prod(w, "ImAzAperp",                       "AzAperp_basis")
+                                                          , prod(w, "ReAzApar",            "qtag_","C","AzApar_basis") )));
+        import(w, RooAddition_("f_sinh","f_sinh",RooArgSet( prod(w, "NAzAz",       "Minus",        "D","AzAz_basis")
+                                                          , prod(w, "NAparApar",   "Minus",        "D","AparApar_basis")
+                                                          , prod(w, "NAperpAperp",                 "D","AperpAperp_basis")
+                                                          , prod(w, "ReAparAperp",         "qtag_","S","AparAperp_basis")
+                                                          , prod(w, "ReAzAperp",           "qtag_","S","AzAperp_basis")
+                                                          , prod(w, "ReAzApar",    "Minus",        "D","AzApar_basis") )));
+        import(w, RooAddition_("f_sin" ,"f_sin", RooArgSet( prod(w, "NAzAz",       "Minus","qtag_","S","AzAz_basis")
+                                                          , prod(w, "NAparApar",   "Minus","qtag_","S","AparApar_basis")
+                                                          , prod(w, "NAperpAperp",         "qtag_","S","AperpAperp_basis")
+                                                          , prod(w, "ReAparAperp", "Minus",        "D","AparAperp_basis")
+                                                          , prod(w, "ReAzAperp",   "Minus",        "D","AzAperp_basis")
+                                                          , prod(w, "ReAzApar",    "Minus","qtag_","S","AzApar_basis") )));
         RooResolutionModel *res = dynamic_cast<RooResolutionModel*>( w.function("res") );
         w.import( RooBDecay(name,name,   get<RooRealVar>(w,"t") , get<RooAbsReal>(w,"tau"), get<RooAbsReal>(w,"dG")
                                        , get<RooAbsReal>(w,"f_cosh"), get<RooAbsReal>(w,"f_sinh") , get<RooAbsReal>(w,"f_cos"), get<RooAbsReal>(w,"f_sin")
@@ -133,7 +129,8 @@ void test_add() {
     // TODO: write things in terms of x & y instead of dG and dm...
     w.factory("{tau[1.5,0.5,2.5],dm[5]}");
     w.factory("RooFormulaVar::dG('@0/@1',{dGG[0],tau})"); 
-    w.factory("RooGaussModel::res(t,mu[0],sigma[0.05])");
+    //w.factory("RooGaussModel::res(t,mu[0],sigma[0.05])");
+    w.factory("RooTruthModel::res(t)");
 
     RooAbsPdf& pdf = jpsiphi(w,"pdf");
     cout << "PDF:" << endl;
@@ -188,9 +185,9 @@ void test_add() {
     }
 
     RooMsgService::instance().addStream(RooFit::DEBUG,Topic(RooFit::Generation));
-    RooMsgService::instance().addStream(RooFit::DEBUG,Topic(RooFit::Plotting));
-    RooMsgService::instance().addStream(RooFit::DEBUG,Topic(RooFit::NumIntegration));
-    //RooMsgService::instance().addStream(RooFit::DEBUG,Topic(RooFit::Integration));
+    //RooMsgService::instance().addStream(RooFit::DEBUG,Topic(RooFit::Plotting));
+    RooMsgService::instance().addStream(RooFit::INFO,Topic(RooFit::NumIntegration));
+    RooMsgService::instance().addStream(RooFit::INFO,Topic(RooFit::Integration));
 
     c->Divide(5,4);
     const char *rname[] = { "rz","rpar","rperp",0 };
@@ -206,10 +203,10 @@ void test_add() {
         }
         RooAbsData *data = pdf.generate(w.argSet("qtag,cpsi,ctheta,phi,t"),10000);
         if (i==0) pdf.fitTo(*data,RooFit::NumCPU(7));
-        RooPlot *p1 = w.var("cpsi")->frame();   /*data->plotOn(p1);*/ pdf.plotOn(p1); c->cd(i*5+1); p1->Draw();
-        RooPlot *p2 = w.var("ctheta")->frame(); /*data->plotOn(p2);*/ pdf.plotOn(p2); c->cd(i*5+2); p2->Draw();
-        RooPlot *p3 = w.var("phi")->frame();    /*data->plotOn(p3);*/ pdf.plotOn(p3); c->cd(i*5+3); p3->Draw();
-        RooPlot *p4 = w.var("t")->frame();      /*data->plotOn(p4);*/ pdf.plotOn(p4); c->cd(i*5+4); p4->Draw();
-        RooPlot *p5 = w.var("t")->frame();      /*data->plotOn(p5, tagAsym );*/ pdf.plotOn(p5, tagAsym); c->cd(i*5+5); p5->Draw();
+        RooPlot *p1 = w.var("cpsi")->frame();   data->plotOn(p1); pdf.plotOn(p1); c->cd(i*5+1); p1->Draw();
+        RooPlot *p2 = w.var("ctheta")->frame(); data->plotOn(p2); pdf.plotOn(p2); c->cd(i*5+2); p2->Draw();
+        RooPlot *p3 = w.var("phi")->frame();    data->plotOn(p3); pdf.plotOn(p3); c->cd(i*5+3); p3->Draw();
+        RooPlot *p4 = w.var("t")->frame();      data->plotOn(p4); pdf.plotOn(p4); c->cd(i*5+4); p4->Draw();
+        RooPlot *p5 = w.var("t")->frame();      data->plotOn(p5, tagAsym ); pdf.plotOn(p5, tagAsym); c->cd(i*5+5); p5->Draw();
     }
 }
