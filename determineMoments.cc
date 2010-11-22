36d90957473b3f4fe1ee18f7ebe4dd86e3aaa4d1
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2010-11-22 17:04:55 +0100

    describe background with moments

--- determineMoments.cc
+++ determineMoments.cc
@@ -1,12 +1,51 @@
 #include "basis.h"
 #include "utils.h"
 #include "TFile.h"
+#include "TCanvas.h"
 #include "RooAbsData.h"
+#include "RooAbsPdf.h"
+#include "RooRealSumPdf.h"
+#include "RooPlot.h"
 #include <stdlib.h>
 
-class moment {
+class IMoment {
+    public:
+          virtual ~IMoment() {};
+          virtual void inc(bool accept = true) = 0 ;
+          virtual ostream& print(ostream&) const = 0 ;
+          virtual RooAbsReal& basis() = 0;
+          virtual double coefficient() const = 0;
+};
+
+class Moment : public IMoment {
 public:
-    moment(const RooAbsReal& x, const RooAbsPdf& pdf, const RooArgSet& nset) : _x(x),_pdf(pdf), _nset(nset), _m0(0),_m1(0),_m2(0) {}
+    Moment(RooAbsReal& x, double c ) : _x(x), _m0(0),_m1(0),_m2(0), _c(c) {}
+
+    void inc(bool accepted=true) {
+        double x = _x.getVal();
+        // TODO: make a histogram of x... (two, one for accept, one for all)
+        ++_m0;
+        if (accepted) {
+            _m1 += x;
+            _m2 += x*x;
+        }
+    }
+
+    ostream& print(ostream& os) const {
+        double mu = _m1/_m0;
+        double sig2 = _m2/_m0 - mu*mu;
+        return os << "moment("<< _x.GetName() << ") = " << mu << " +- " << sqrt(sig2/(_m0-1)) << " significance: " << mu/sqrt(sig2/_m0) << endl;
+    }
+
+    RooAbsReal& basis() { return _x; }
+    double coefficient() const { return _c*_m1/_m0; }
+private:
+    RooAbsReal& _x;
+    double _m0,_m1,_m2,_c;
+};
+class EffMoment  : public IMoment{
+public:
+    EffMoment(RooAbsReal& x, const RooAbsPdf& pdf, const RooArgSet& nset) : _x(x),_pdf(pdf), _nset(nset), _m0(0),_m1(0),_m2(0) {}
 
     void inc(bool accepted=true) {
         double x = _x.getVal()/_pdf.getVal(&_nset);
@@ -23,8 +62,11 @@ public:
         double sig2 = _m2/_m0 - mu*mu;
         return os << "moment("<< _x.GetName() <<"_" << _pdf.GetName() << ") = " << mu << " +- " << sqrt(sig2/(_m0-1)) << " significance: " << mu/sqrt(sig2/_m0) << endl;
     }
+
+    RooAbsReal& basis() { return _x; }
+    double coefficient() const { return _m1/_m0; }
 private:
-    const RooAbsReal& _x;
+    RooAbsReal& _x;
     const RooAbsPdf& _pdf;
     const RooArgSet&  _nset;
     double _m0,_m1,_m2;
@@ -63,23 +105,26 @@ void determineMoments(const char* fname="p2vv.root", const char* pdfName = "pdf"
    // marginalize pdf over 'the rest' so we get the normalization of the moments right...
    RooAbsPdf *pdf_marginal = pdf->createProjection(*marginalObs);
 
+
    abasis ab(*w,"cpsi","ctheta","phi");
    eps efficiency( get<RooAbsReal>(*w,"cpsi")
                  , get<RooAbsReal>(*w,"ctheta")
                  , get<RooAbsReal>(*w,"phi"));
              
-   std::vector<moment*> moments;
-   typedef std::vector<moment*>::iterator moments_iterator; 
-   for (int i=0;i<5;++i) {
-     for (int l=0;l<5;++l) {
+   std::vector<IMoment*> moments;
+   typedef std::vector<IMoment*>::iterator moments_iterator; 
+   for (int i=0;i<4;++i) {
+     for (int l=0;l<4;++l) {
         for (int m=0;m<=l;++m) {
-            moments.push_back(new moment( ab("mom",i,0,l,m,double(2*i+1)/2 ), *pdf_marginal, *allObs ) );
+            // if we want to write it as efficiency, i.e. eps_ijk * P_i * Y_jk * PDF then we need the marginal..
+            // moments.push_back(new EffMoment( ab("mom",i,0,l,m,double(2*i+1)/2 ), *pdf_marginal, *allObs ) );
+            moments.push_back(new Moment( ab("mom",i,0,l,m,double(2*i+1)/2.), 2./(2*i+1)  ) );
         }
      }
    }
 
    // loop over all data
-   for (int i=0;i<data->numEntries(); ++i) {
+   for (int i=0;i<data->numEntries()/10; ++i) {
         const RooArgSet *args = data->get(i);
         *allObs  = *args;
         // apply some fake efficiency, and see how it affects the moments...
@@ -89,4 +134,21 @@ void determineMoments(const char* fname="p2vv.root", const char* pdfName = "pdf"
 
    // and print the results:
    for ( moments_iterator m = moments.begin(); m!=moments.end(); ++m) (*m)->print(cout);
+
+
+   RooArgList coef,fact;
+   for ( moments_iterator m = moments.begin(); m!=moments.end(); ++m) {
+       const char *name = Format("C_%f",(*m)->coefficient());
+       w->factory(Format("%s[%f]",name,(*m)->coefficient()));
+       coef.add(get<RooAbsReal>(*w,name));
+       fact.add((*m)->basis());
+   }
+   RooAbsPdf *p = new RooRealSumPdf("pdf_mom","pdf_mom",fact,coef);
+
+   const char *cvar[] = { "cpsi","ctheta","phi" };
+   TCanvas *c = new TCanvas();
+   c->Divide(3,1);
+   for (int i = 0; i<3; ++i) {
+        c->cd(1+i); RooPlot *plot = get<RooRealVar>(*w,cvar[i]).frame(); data->plotOn(plot); p->plotOn(plot); plot->Draw();
+   }
 }
