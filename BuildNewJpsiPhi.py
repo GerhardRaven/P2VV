c83e4691e525c16db09c8bf311b68970f1368463
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2010-12-02 20:42:26 +0100

    improve plots a bit more

--- BuildNewJpsiPhi.py
+++ BuildNewJpsiPhi.py
@@ -6,8 +6,7 @@ from ModelBuilders import buildJpsiphi,buildJpsikstar
 from math import pi
 
 ws = RooWorkspace('ws')
-RooWorkspace.put = getattr(RooWorkspace,'import') # import is a reserved word, so we call it 'put' from now on...
-
+# create observables
 ws.factory("{ trcospsi[-1,1], trcostheta[-1,1], trphi[%f,%f], t[-3,12], tagdecision[Bs_Jpsiphi=+1,Bsbar_Jpsiphi=-1], qrec[jpsikstar=-1,jpsikstarbar=1]}"%(-pi,pi))
 
 ##choice: either fit for the Re&Im of the 3 amplitudes (and then
@@ -16,7 +15,6 @@ ws.factory("{ trcospsi[-1,1], trcostheta[-1,1], trphi[%f,%f], t[-3,12], tagdecis
 ##         Note: initial values from arXiv:0704.0522v2 [hep-ex] BaBar PUB-07-009
 
 ws.factory("{rz[0.556],rpar[0.211],rperp[0.233]}")
-#ws.factory("{deltaz[0],deltapar[-2.93,-3.15,3.15],deltaperp[2.91,-3.15,3.15]}")
 ws.factory("{deltaz[0],deltapar[-2.93],deltaperp[2.91]}")
 ws.factory("expr::ReAz   ('rz    * cos(deltaz)',   {rz,deltaz})")
 ws.factory("expr::ImAz   ('rz    * sin(deltaz)',   {rz,deltaz})")
@@ -25,7 +23,7 @@ ws.factory("expr::ImApar ('rpar  * sin(deltapar)', {rpar,deltapar})")
 ws.factory("expr::ReAperp('rperp * cos(deltaperp)',{rperp,deltaperp})")
 ws.factory("expr::ImAperp('rperp * sin(deltaperp)',{rperp,deltaperp})")
 
-ws.factory("{gamma[0.68,0.4,0.9],dm[17.7],dG[0.075,-0.3,0.3]}")
+ws.factory("{gamma[0.68,0.4,0.9],dm[17.7],dG[0.45,-0.5,0.5]}")
 ws.factory("expr::tau('1/@0',{gamma})")
 
 ws.factory("RooGaussModel::res(t,mu[0],sigma[0.05])")
@@ -49,18 +47,23 @@ ws.factory("{expr::S('sin(phis)',{phis[0.8]}),expr::D('cos(phis)',{phis}),C[0]}"
 pdf = buildJpsiphi(ws,'jpsiphipdf')  ## for now we rely quite a bit on a naming convention -- 
                                       ## in future we should pass more information into the builder
 
+
+
+### let's make some nice plots to show what this PDF looks like...
+
 obsNames =[ 'trcospsi','trcostheta','trphi','t','tagdecision' ]
 obs = ws.argSet(','.join(obsNames))
 
 canvas = TCanvas('canvas','canvas')
 canvas.Divide(5,4)
 
-
 p = [ 'rz','rpar','rperp' ]
 for i in  range(len(p)+1) :
     if i>0 : 
-        for j in range(len(p)) : ws.var(p[j]).setVal( 1 if i==j+1 else 0 )
-    #for l in p : ws.var(l).Print()
+        for j in range(len(p)) : 
+            x = ws.var(p[j])
+            x.setVal( 1 if i==j+1 else 0 )
+    for l in p + ['deltaz','deltapar','deltaperp' ] : ws.var(l).Print()
 
     for (j,k) in zip(['trcospsi','trcostheta','trphi','t'],range(5*i+1,100)) :
        canvas.cd(k)
@@ -78,3 +81,4 @@ for i in  range(len(p)+1) :
     pdf.plotOn(f,RooFit.Project(proj),tagAsym)
     f.Draw()
 
+canvas.Flush()
