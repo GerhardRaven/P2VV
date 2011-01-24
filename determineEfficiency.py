a825a0b5b9b8fb0ec92d912e416f650a341f57bb
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-01-24 15:24:41 +0100

    make determineEfficiency a lot faster by re-ordering things a bit

--- determineEfficiency.py
+++ determineEfficiency.py
@@ -43,42 +43,57 @@ marginalObs = pdf.getObservables( data.get() )
 marginalObs.remove( angles )
 # marginalize pdf over 'the rest' so we get the normalization of the moments right...
 pdf_marginal = pdf.createProjection(marginalObs)
-moments = []
 
+# compute the 'canonical' six moments
+bnames = [ 'AzAz','AparApar','AperpAperp','AparAperp','AzAperp','AzApar' ]
+sixmom = [ EffMoment( w['%s_basis'%n], 1., pdf_marginal, allObs ) for n in bnames ]
+computeMoments(data,sixmom)
+xi_m = dict( [ (m.basis().GetName(),m.coefficient()) for m in sixmom ] )
+
+
+# compute the Fourier series for the efficiency
+moments = []
 ab = abasis(w,angles)
-for (i,l) in product(range(6),range(4)) :
+for (i,l) in product(range(10),range(4)) :
     # if we want to write it as efficiency, i.e. eps_ijk * P_i * Y_jk * PDF then we need the marginal..
     # Warning: the Y_lm are orthonormal, but the P_i are orthogonal, but the dot product is (2*i+1)/2
     moments += [ EffMoment( ab.build("mom",i,0,l,m,1. ),float(2*i+1)/2, pdf_marginal, allObs ) for m in range(-l,l+1) ]
 
-
 # loop over all data, determine moments
-pdf_eff = buildEffMomentsPDF(w, "_eff", pdf, data, moments)
+computeMoments(data,moments)
 
 
-# compare with the canonical six moments
-bnames = [ 'AzAz','AparApar','AperpAperp','AparAperp','AzAperp','AzApar' ]
-sixmom = [ EffMoment( w['%s_basis'%n], 1., pdf_marginal, allObs ) for n in bnames ]
-computeMoments(data,sixmom)
-
-# 
+# compute the 'canonical' moments given the Fourier series
 c = dict()
 for m in moments :
    c[ ( m.basis().i(),m.basis().l(),m.basis().m() ) ] = m.coefficient()
 
 from math import sqrt,pi
-xi = { 'AparApar_basis'   :  8*sqrt(pi)/9 * ( c[(0,0,0)]-  c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]-  c[(2,2,0)]/5) + sqrt(3./20)*(c[(0,2,2)]-  c[(2,2,2)]/5)  )
-     , 'AzAz_basis'       :  8*sqrt(pi)/9 * ( c[(0,0,0)]+2*c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]+2*c[(2,2,0)]/5) - sqrt(3./20)*(c[(0,2,2)]+2*c[(2,2,2)]/5)  )
-     , 'AperpAperp_basis' :  8*sqrt(pi)/9 * ( c[(0,0,0)]-  c[(2,0,0)]/5 - sqrt(1./ 5)*( c[(0,2,0)]-  c[(2,2,0)]/5 ) )
-     , 'AparAperp_basis'  :  8*sqrt(pi)/9 * sqrt(3./5.)*( c[(0,2,-1)] - c[(2,2,-1)]/5 )
-     , 'AzAperp_basis'    : -8*sqrt(pi)/9 * sqrt(6./5.)* 3*pi/32 *( c[(1,2,1)] - c[(3,2,1)]/4 - 5*c[(5,2,1)]/128 ) 
-     , 'AzApar_basis'     :  8*sqrt(pi)/9 * sqrt(6./5.)* 3*pi/32 *( c[(1,2,-2)] - c[(3,2,-2)]/4  -5*c[(5,2,-2)]/128)
-     }
+xi_c = { 'AparApar_basis'   :   ( c[(0,0,0)]-  c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]-  c[(2,2,0)]/5) + sqrt(3./20)*(c[(0,2,2)]-  c[(2,2,2)]/5)  )
+       , 'AzAz_basis'       :   ( c[(0,0,0)]+2*c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]+2*c[(2,2,0)]/5) - sqrt(3./20)*(c[(0,2,2)]+2*c[(2,2,2)]/5)  )
+       , 'AperpAperp_basis' :   ( c[(0,0,0)]-  c[(2,0,0)]/5 - sqrt(1./ 5)*( c[(0,2,0)]-  c[(2,2,0)]/5 ) )
+       , 'AparAperp_basis'  :   sqrt(3./5.)*( c[(0,2,-1)] - c[(2,2,-1)]/5 )
+       , 'AzAperp_basis'    : - sqrt(6./5.)* 3*pi/32 *( c[(1,2, 1)] - c[(3,2, 1)]/4 - 5*c[(5,2, 1)]/128  - 7*c[(7,2, 1)]/512 - 105*c[(9,2, 1)]/16484) 
+       , 'AzApar_basis'     :   sqrt(6./5.)* 3*pi/32 *( c[(1,2,-2)] - c[(3,2,-2)]/4 - 5*c[(5,2,-2)]/128  - 7*c[(7,2,-2)]/512 - 105*c[(9,2,-2)]/16484)
+       }
 
-for m in sixmom :
-    name = m.basis().GetName()
-    print '%s : moment: %s  computed: %s ; ratio = %s ' % ( name, m.coefficient(), xi[name], m.coefficient()/xi[name] )
+# normalize moments and compare
+def norm_xi( d ) :
+    n =  (d['AparApar_basis'] + d['AzAz_basis'] + d['AperpAperp_basis'])/3
+    for i in d.iterkeys() : d[i] = d[i]/n
 
+norm_xi(xi_c)
+norm_xi(xi_m)
+for name in xi_c.iterkeys() :
+    print '%s : direct moment: %s ;  moment computed from Fourier series: %s ; ratio = %s ' % ( name, xi_m[name], xi_c[name], xi_m[name]/xi_c[name])
+
+
+## build PDF using the Fourier series efficiency...
+pdf_eff = buildEff_x_PDF(w,'_eff',pdf,[ ( m.basis() , m.coefficient() ) for m in moments ] )
+
+## build PDF using Fourier coefficients reverse engineered from the six moments...
+
+### make some plots...
 c = TCanvas()
 c.Divide(3,1);
 for (i,var) in enumerate(angles) :
