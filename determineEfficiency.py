f3d46066e826e0007a5f386f020e0fa0db2caa5a
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-01-24 14:13:10 +0100

    extend determineEfficiency to also compute the six canonical moments, and compare them to expectation of the c_ijk

--- determineEfficiency.py
+++ determineEfficiency.py
@@ -13,7 +13,7 @@ class efficiency :
         from math import cos
         z = -1+2*cos( self.phi.getVal() )
         from random import random
-        return random() > ( x*x*y/( 1 if z<0 else 1-z ) )
+        return random() > ( x*x*y ) # /( 1 if z<0 else 1-z ) )
 
 fname="p2vv_9.root"
 pdfName = "jpsiphipdf"
@@ -26,8 +26,8 @@ pdf = w[pdfName]
 data = w[dataName] 
 allObs = pdf.getObservables( data.get() )
 
-#angles = w.argSet("transversityangles")
-angles = w.set("helicityangles")
+angles = w.set("transversityangles")
+#angles = w.set("helicityangles")
 
 #replace input by inefficient data
 eps = efficiency( angles )
@@ -46,14 +46,38 @@ pdf_marginal = pdf.createProjection(marginalObs)
 moments = []
 
 ab = abasis(w,angles)
-for (i,l) in product(range(4),range(4)) :
+for (i,l) in product(range(6),range(4)) :
     # if we want to write it as efficiency, i.e. eps_ijk * P_i * Y_jk * PDF then we need the marginal..
     # Warning: the Y_lm are orthonormal, but the P_i are orthogonal, but the dot product is (2*i+1)/2
     moments += [ EffMoment( ab.build("mom",i,0,l,m,1. ),float(2*i+1)/2, pdf_marginal, allObs ) for m in range(-l,l+1) ]
 
+
 # loop over all data, determine moments
 pdf_eff = buildEffMomentsPDF(w, "_eff", pdf, data, moments)
 
+
+# compare with the canonical six moments
+bnames = [ 'AzAz','AparApar','AperpAperp','AparAperp','AzAperp','AzApar' ]
+sixmom = [ EffMoment( w['%s_basis'%n], 1., pdf_marginal, allObs ) for n in bnames ]
+computeMoments(data,sixmom)
+
+# 
+c = dict()
+for m in moments :
+   c[ ( m.basis().i(),m.basis().l(),m.basis().m() ) ] = m.coefficient()
+
+from math import sqrt,pi
+xi = { 'AparApar_basis'   :  8*sqrt(pi)/9 * ( c[(0,0,0)]-  c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]-  c[(2,2,0)]/5) + sqrt(3./20)*(c[(0,2,2)]-  c[(2,2,2)]/5)  )
+     , 'AzAz_basis'       :  8*sqrt(pi)/9 * ( c[(0,0,0)]+2*c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]+2*c[(2,2,0)]/5) - sqrt(3./20)*(c[(0,2,2)]+2*c[(2,2,2)]/5)  )
+     , 'AperpAperp_basis' :  8*sqrt(pi)/9 * ( c[(0,0,0)]-  c[(2,0,0)]/5 - sqrt(1./ 5)*( c[(0,2,0)]-  c[(2,2,0)]/5 ) )
+     , 'AparAperp_basis'  :  8*sqrt(pi)/9 * sqrt(3./5.)*( c[(0,2,-1)] - c[(2,2,-1)]/5 )
+     , 'AzAperp_basis'    : -8*sqrt(pi)/9 * sqrt(6./5.)* 3*pi/32 *( c[(1,2,1)] - c[(3,2,1)]/4 - 5*c[(5,2,1)]/128 ) 
+     , 'AzApar_basis'     :  8*sqrt(pi)/9 * sqrt(6./5.)* 3*pi/32 *( c[(1,2,-2)] - c[(3,2,-2)]/4  -5*c[(5,2,-2)]/128)
+     }
+
+for m in sixmom :
+    name = m.basis().GetName()
+    print '%s : moment: %s  computed: %s ; ratio = %s ' % ( name, m.coefficient(), xi[name], m.coefficient()/xi[name] )
 
 c = TCanvas()
 c.Divide(3,1);
@@ -66,3 +90,21 @@ for (i,var) in enumerate(angles) :
      plot.Draw()
 c.Flush()
 
+#
+#xi = { 'parpar' : 1
+#     , '00'     : 1
+#     , 'perpperp' : 1
+#     , 'perp0'  : 0,
+#     , 'par0'   : 0,
+#     , 'parperp' : 0 ] # TODO: verify signconvention!!!
+#
+#coef = [ (0,0,0,   ( xi['parpar']+xi['00']+xi['perpperp'])/3 )
+#       , (2,0,0,   ( xi['00']-xi['parpar'] )*float(5)/3      )
+#       , (0,2,0,   ( xi['parpar']-xi['perpperp'] ) * sqrt(float(20)/9) )
+#       , (0,2,-1,  ( xi['parperp'] ) * sqrt(float(5)/3) )
+#       , (1,2,1,   ( xi['perp0'] ) * sqrt(float(5)/6) * float(32)/(3*pi) )
+#       , (1,2,-1 , ( xi['par0'] ) * sqrt(float(5)/6)*float(32)/(3*pi) )
+#       ]
+#
+# pdf = buildEff_x_PDF(w,'eff',pdf,[ ab.build('mom_eff',c[0],0,c[1],c[2],1.), c[3] for c in coef ] )
+       
