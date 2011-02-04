8dfd5fd0e3f2e0bbdb5711b0d582e3d7501b503e
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-02-04 13:25:41 +0100

    normalize Fourier series & add exact Fourier describable eff

--- determineEfficiency.py
+++ determineEfficiency.py
@@ -5,6 +5,8 @@ from ROOT import *
 gSystem.Load("libp2vv.so")
 from math import sqrt,pi
 
+NumCPU = RooCmdArg(RooFit.NumCPU(8))
+
 def compute_moments( c ) :
     return { 'AparApar_basis'   :   ( c[(0,0,0)]-  c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]-  c[(2,2,0)]/5) + sqrt(3./20)*(c[(0,2,2)]-  c[(2,2,2)]/5)  )
            , 'AzAz_basis'       :   ( c[(0,0,0)]+2*c[(2,0,0)]/5 + sqrt(1./20)*( c[(0,2,0)]+2*c[(2,2,0)]/5) - sqrt(3./20)*(c[(0,2,2)]+2*c[(2,2,2)]/5)  )
@@ -14,17 +16,25 @@ def compute_moments( c ) :
            , 'AzApar_basis'     :   sqrt(6./5.)* 3*pi/32 *( c[(1,2,-2)] - c[(3,2,-2)]/4 ) #- 5*c[(5,2,-2)]/128  - 7*c[(7,2,-2)]/512 - 105*c[(9,2,-2)]/16484)
            }
 
+def legendre(x) :
+        return (1,x,0.5*(3*x*x-1),0.5*(5*x*x*x-3*x) )
+def spharmonic(ct,phi) :
+        st = sqrt(1-ct*ct)
+        x=st*cos(phi)
+        y=st*sin(phi)
+        z=ct
+        n0 = sqrt(1./(4*pi))
+        n1 = sqrt(3./(4*pi))
+        return ( (n0,), (-n1*y,n1*z,-n1*x),  )  # 0,1,2  -> -1,0,+1
 
 class efficiency :
     def __init__(self,*args) : 
         (self.cpsi,self.ctheta,self.phi) = args[0] if len(args)==1 else args
     def accept(self) :
-        x = self.cpsi.getVal()
-        y = self.ctheta.getVal()
-        from math import cos
-        z = -1+2*cos( self.phi.getVal() )
+        p = legendre( self.cpsi.getVal() )
+        y = spharmonic( self.ctheta.getVal(), self.phi.getVal() )
         from random import random
-        return random() > ( x*x*y ) # /( 1 if z<0 else 1-z ) )
+        return random() < ( p[0]+0.2*p[1]+0.4*p[2] )/3 * ( y[0][0] + 0.1*y[1][-1+1] + 0.2*y[1][0+1] + 0.3*y[1][1+1] ) 
 
 fname="p2vv_10.root"
 pdfName = "jpsiphipdf"
@@ -80,6 +90,7 @@ computeMoments(data,moments)
 # compute the 'canonical' moments given the Fourier series
 c = dict()
 for m in moments : c[ ( m.basis().i(),m.basis().l(),m.basis().m() ) ] = m.coefficient()
+c_000 = c[(0,0,0)]
 
 xi_c = compute_moments( c )
 # normalize moments and compare
@@ -96,15 +107,15 @@ for name in xi_c.iterkeys() :
 ## build PDF using the Fourier series efficiency...
 ## TODO: normalize relative to 0000 so that c_000 = 1
 print 'using the following terms in Fourier expansion: '
-for n,c in [ ( m.basis().GetName() , m.coefficient() ) for m in moments if m.significance()>2] :
+for n,c in [ ( m.basis().GetName() , m.coefficient()/c_000 ) for m in moments if m.significance()>2] :
     print '%s : %s ' % (n,c)
 # now compute the six moments with the terms we'll use only (i.e. put the other coefficients to zero)
 c_sup = dict()
 for m in moments :
-   c_sup[ ( m.basis().i(),m.basis().l(),m.basis().m() ) ] = m.coefficient() if m.significance()>2 else 0
+   c_sup[ ( m.basis().i(),m.basis().l(),m.basis().m() ) ] = m.coefficient()/c_000 if m.significance()>2 else 0
 xi_c_sup = compute_moments( c_sup )
 
-pdf_eff = buildEff_x_PDF(w,'fourier_eff',pdf,[ ( m.basis() , m.coefficient() ) for m in moments if m.significance()>2] )
+#pdf_eff = buildEff_x_PDF(w,'fourier_eff',pdf,[ ( m.basis() , m.coefficient()/c_000 ) for m in moments if m.significance()>2] )
 
 ## build PDF using Fourier coefficients reverse engineered from the six moments...
 #
@@ -123,19 +134,19 @@ pdf_mom = buildEff_x_PDF(w,'reve_mom_eff',pdf,[ ( ab.build('reve_mom_eff',c[0],0
 print '*'*80
 print '*'*20 + ' fitting original PDF on original data'
 print '*'*80
-pdf.fitTo(origdata,RooFit.NumCPU(2))
+pdf.fitTo(origdata,NumCPU)
 print '*'*80
 print '*'*20 + ' fitting original PDF on inefficient data'
 print '*'*80
-pdf.fitTo(data,RooFit.NumCPU(2))
+pdf.fitTo(data,NumCPU)
 print '*'*80
 print '*'*20 + ' fitting reverse engineerd moment PDF on inefficient data'
 print '*'*80
-pdf_mom.fitTo(data,RooFit.NumCPU(2))
-print '*'*80
-print '*'*20 + ' fitting Fourier expansion efficiency PDF on inefficient data'
-print '*'*80
-pdf_eff.fitTo(data,RooFit.NumCPU(2))
+pdf_mom.fitTo(data,NumCPU)
+#print '*'*80
+#print '*'*20 + ' fitting Fourier expansion efficiency PDF on inefficient data'
+#print '*'*80
+#pdf_eff.fitTo(data,NumCPU)
 
 ### make some plots...
 c = TCanvas()
@@ -145,7 +156,7 @@ for (i,var) in enumerate(angles) :
      plot = var.frame()
      data.plotOn(plot); 
      pdf.plotOn(plot,RooFit.LineColor(kBlue))
-     pdf_eff.plotOn(plot,RooFit.LineColor(kRed))
+     #pdf_eff.plotOn(plot,RooFit.LineColor(kRed))
      pdf_mom.plotOn(plot,RooFit.LineColor(kOrange))
      plot.Draw()
 c.Flush()
