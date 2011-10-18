874323bba5b07f769a65693d570fdffe40ed298e
Roel Aaij <roel.aaij@gmail.com>
2011-10-18 10:38:54 +0200

    Ongoing work on TimeAngles

--- python/TimeAnglesPdf.py
+++ python/TimeAnglesPdf.py
@@ -1,31 +1,59 @@
 from RooFitWrappers import Pdf
 
-class TimeAnglesPdf( Pdf ):
-    def __init__(self, name, Observables, AngularIndeces, ResolutionModel):
+class TimeAnglesPdf(Pdf):
+    def __init__(self, name, Observables, TimeComponents, AngularIndeces, Parameters, ResolutionModel):
         if len(Observables) != 4:
             raise StandardError('Need four observables: time and three angles')
+
+        diff = set(TimeComponents.keys()).symmetric_difference(set(AngularIndeces.keys()))
+        if len(diff) != 0:
+            print 'Error, time and angular components do not contain the same labels: %s' % diff
+            raise RuntimeError()
         
         self._angles = Observables[1:]
 
-        for n, d in AngularIndeces:
-            self.__angular_function(n, d)
+        self._angle_functions = {}
+        for l, (n, i) in AngularIndeces.iteritems():
+            self._angle_functions[l] = self.__angular_function(n, i)
 
-        super(TimeAnglesPdf, self).__init__(name, o, **d))
+        d = {}
+        d['Observables'] = Observables
+        # Put the names of the terms in the Parameters
+        d['Parameters'] = Parameters[:-1] + TimeComponents[TimeComponents.keys()[0]].keys() \
+                          + Parameters[-1:]
+        d['ResolutionModel'] = ResolutionModel
+        
+        super(TimeAnglesPdf, self).__init__(name, **d))
 
     def __angular_function(self, n, d):
-        if n in self.ws(): return self.ws()[ n ]
+        if n in self.ws()._objects:
+            return self.ws()._objects[n]
         functions = [self.__basis_function( n, *c ) for c in d]
-        return self._declare('sum::%s(%s)' % tuple(n, ','.join( functions )))
+        return self._declare('sum::%s(%s)' % tuple(n, ','.join(functions)))
 
     def __basis_function(self, name, i, j, k, l, c):
         # construct name
         name = '%s_P%d%dY%d%d' % tuple([name, i, j, k, l])
         # build basis function
-        if name not in self.ws():
-            self._declare('P2VVAngleBasis::%s(%s, %s, %s, %d, %d, %d, %d, %f)' \
-                          % tuple([name] + [a.GetName() for a in self._angles]
-                                  + [i, j, k, l, c]))
+        if name not in self.ws()._objects:
+            bf = self._declare('P2VVAngleBasis::%s(%s, %s, %s, %d, %d, %d, %d, %f)' \
+                               % tuple([name] + [a.GetName() for a in self._angles]
+                                       + [i, j, k, l, c]))
+            self.ws()._objects[name] = bf
         return name
+
+    def _makeRecipe(self, variables):
+        terms = self._time_components[self._time_components.keys()[0]].keys()
+        sums = {}
+        for term in terms:
+            s = Zero
+            for label in self._time_components.iterkeys():
+                s += self._time_components[label][term] * self._angle_functions[label][term]
+            sums[term] = s
+        variables = variables[:-1] + [s['Name'] for s in sums] + variables[-1:]
+        deps = ','.join([v.GetName() if type(v) != str else v for v in variables])
+        return "BDecay::%s(%s, %s, %s, %s, %s, %s, %s, %s,\
+        %s, SingleSided)" % deps
 
 ## Definition of angular functions in helicity basis.
 ## angFuncs.append(('J_0020x0020_0', [ ( 0, 0, 0,  0,  4.               ),
