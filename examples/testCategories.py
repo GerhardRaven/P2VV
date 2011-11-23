e884b69450ab59771f25aee55545da2cb3a90751
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-11-23 09:59:48 +0100

    improve testCategories

--- examples/testCategories.py
+++ examples/testCategories.py
@@ -1,12 +1,17 @@
 from RooFitWrappers import *
-ws = RooObject ( workspace = 'ws' )
-x = Category('mystate', States = [ 'aap','noot','mies' ] )
+ws = RooObject ( workspace = 'myws' )
+x = Category('mystate', States = [ 'aap','noot','mies','vuur' ] )
 y = Category('mystate2', States = [ 'x','y' ] )
 z = SuperCategory('sc',[x,y])
-z.Print("V")
-for i in z.states() : print i
+#z.Print("V")
+for i in x.states() : print i
 a = MappedCategory('mc',x,{'one':('aap','noot')
                           ,'two':('mies', ) } )
 
 for i in a.states() : print i
-a.Print("V")
+#a.Print("V")
+
+
+for i in x.states() :
+    x.setLabel(i)
+    print x.getLabel(),a.getLabel()
