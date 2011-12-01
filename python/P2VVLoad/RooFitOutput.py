22de168d4fd93e3f641a4cfa661440a27ff69de3
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-01 09:25:52 +0100

    use wrappers for stream

--- python/P2VVLoad/RooFitOutput.py
+++ python/P2VVLoad/RooFitOutput.py
@@ -9,19 +9,22 @@ from ROOT import RooFit, RooMsgService
 # get message service instance
 msgServ = RooMsgService.instance()
 
-# remove all output streams
-for stream in range(msgServ.numStreams()) : msgServ.deleteStream(stream)
+for stream in msgServ :
+    if stream.minLevel == RooFit.INFO : stream -= RooFit.Plotting
 
-# add default streams for P2VV
-msgServ.addStream(RooFit.PROGRESS)
-msgServ.addStream(RooFit.INFO, RooFit.Topic(RooFit.Minimization))
-#msgServ.getStream(1).addTopic(RooFit.Plotting)
-msgServ.getStream(1).addTopic(RooFit.Fitting)
-msgServ.getStream(1).addTopic(RooFit.Eval)
-msgServ.getStream(1).addTopic(RooFit.Caching)
-msgServ.getStream(1).addTopic(RooFit.ObjectHandling)
-msgServ.getStream(1).addTopic(RooFit.InputArguments)
-msgServ.getStream(1).addTopic(RooFit.DataHandling)
-msgServ.getStream(1).addTopic(RooFit.NumIntegration)
+## remove all output streams
+#for stream in range(msgServ.numStreams()) : msgServ.deleteStream(stream)
+#
+## add default streams for P2VV
+#msgServ.addStream(RooFit.PROGRESS)
+#msgServ.addStream(RooFit.INFO, RooFit.Topic(RooFit.Minimization))
+##msgServ.getStream(1).addTopic(RooFit.Plotting)
+#msgServ.getStream(1) += ( RooFit.Fitting
+#                        , RooFit.Eval
+#                        , RooFit.Caching
+#                        , RooFit.ObjectHandling
+#                        , RooFit.InputArguments
+#                        , RooFit.DataHandling
+#                        , RooFit.NumIntegration )
 
 
