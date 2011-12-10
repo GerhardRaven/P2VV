251a5ba0dd9660736b796d5b643e8184082930a1
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-10 12:10:18 +0100

    update to use latest features

--- examples/acceptance_histo.py
+++ examples/acceptance_histo.py
@@ -9,8 +9,7 @@ style = set_style()
 style.SetOptTitle(1)
 style.SetOptStat(0)
 
-ws = RooObject()
-ws.setWorkspace(RooWorkspace('swimming'))
+ws = RooObject( workspace = 'swimming' )
 
 t = RealVar('tau', Title = 'decay time', Unit='ps', Observable = True, MinMax=(-1.07, 14))
 m = RealVar('m', Title = 'B mass', Unit = 'MeV', Observable = True, MinMax = (5155, 5450))
@@ -23,33 +22,29 @@ from ROOT import RooExponential as Exponential
 from ROOT import RooDecay as Decay
 from ROOT import RooCBShape as CrystalBall
 
-signal_tau = RealVar('signal_tau', Title = 'mean lifetime', Unit = 'ps', Value =  1.5,
-                     MinMax = (1., 2.5))
+signal_tau = RealVar('signal_tau', Title = 'mean lifetime', Unit = 'ps', Value =  1.5, MinMax = (1., 2.5))
 
-mc_res = ResolutionModel('mc_res', Type = RooTruthModel, Observables = [t])
-mcpdf = Pdf('mc_pdf', Type = Decay, Observables = [t], ResolutionModel = mc_res,
-            Parameters = [signal_tau], Options = ['SingleSided'])
+mc_res = ResolutionModel('mc_res', Type = TruthModel, Observables = [t])
+mcpdf = Pdf('mc_pdf', Type = Decay, Observables = [t], ResolutionModel = mc_res, Parameters = [signal_tau], Options = ['SingleSided'])
 
 # Time resolution model
 from P2VVParameterizations.TimeResolution import ResolutionModelLP2011
 tres = ResolutionModelLP2011(t).Model
 
 # Signal time pdf
-sig_t = Pdf('sig_t', Type = Decay, Observables = [t], Parameters = [signal_tau],
-            ResolutionModel = tres, Options = ['SingleSided'])
+sig_t = Pdf('sig_t', Type = Decay, Observables = [t], Parameters = [signal_tau], ResolutionModel = tres, Options = ['SingleSided'])
 
 # B mass pdf
-m_mean  = RealVar('m_mean',  Observable = False, Unit = 'MeV', Value = 5300, MinMax = (5200, 5800))
-m_sigma = RealVar('m_sigma', Observable = False, Unit = 'MeV', Value = 15, MinMax = (10, 30))
+m_mean  = RealVar('m_mean',   Unit = 'MeV', Value = 5300, MinMax = (5200, 5800))
+m_sigma = RealVar('m_sigma',  Unit = 'MeV', Value = 15, MinMax = (10, 30))
 sig_m = Pdf('sig_m', Type = Gaussian, Observables = (m,), Parameters = (m_mean, m_sigma ))
 
 # J/psi mass pdf
-mpsi_mean  = RealVar('mpsi_mean',  Observable = False, Unit = 'MeV', Value = 3097, MinMax = (3070, 3110))
-mpsi_sigma = RealVar('mpsi_sigma', Observable = False, Unit = 'MeV', Value = 10, MinMax = (5, 20))
-mpsi_alpha = RealVar('mpsi_alpha', Observable = False, Unit = '', Value = 1.36, MinMax = (0.5, 3))
-mpsi_n = RealVar('mpsi_n', Observable = False, Unit = '', Value = 1, MinMax = (0.1, 2))
-sig_mpsi = Pdf('sig_mpsi', Type = CrystalBall, Observables = [mpsi],
-               Parameters = [mpsi_mean, mpsi_sigma, mpsi_alpha, mpsi_n])
+mpsi_mean  = RealVar('mpsi_mean',   Unit = 'MeV', Value = 3097, MinMax = (3070, 3110))
+mpsi_sigma = RealVar('mpsi_sigma',  Unit = 'MeV', Value = 10, MinMax = (5, 20))
+mpsi_alpha = RealVar('mpsi_alpha',  Unit = '', Value = 1.36, MinMax = (0.5, 3))
+mpsi_n = RealVar('mpsi_n',  Unit = '', Value = 1, MinMax = (0.1, 2))
+sig_mpsi = Pdf('sig_mpsi', Type = CrystalBall, Observables = [mpsi], Parameters = [mpsi_mean, mpsi_sigma, mpsi_alpha, mpsi_n])
 
 # Create signal component
 signal = Component('signal',{m: sig_m, mpsi: sig_mpsi,t:  sig_t}, Yield= (50000,0,150000) )
@@ -57,24 +52,20 @@ signal = Component('signal',{m: sig_m, mpsi: sig_mpsi,t:  sig_t}, Yield= (50000,
 
 # Create combinatorical background component
 
-m_c = RealVar( 'm_c', Observable = False, Unit = '1/MeV',
-               Value = -0.0004, MinMax = (-0.1, -0.00001))
+m_c = RealVar( 'm_c', Unit = '1/MeV', Value = -0.0004, MinMax = (-0.1, -0.00001))
 bkg_m = Pdf('bkg_m', Observables = [m], Type = Exponential, Parameters = [m_c])
 
-psi_c = RealVar( 'psi_c', Observable = False, Unit = '1/MeV',
-                 Value = -0.0004, MinMax = (-0.1, -0.0000001))
+psi_c = RealVar( 'psi_c',  Unit = '1/MeV', Value = -0.0004, MinMax = (-0.1, -0.0000001))
 bkg_mpsi = Pdf('bkg_mpsi', Observables = [mpsi], Type = Exponential, Parameters = [psi_c])
 
-bkg_tau = RealVar('bkg_tau', Title = 'comb background lifetime', Unit = 'ps', Value = 1,
-                  MinMax = (0.0001, 5))
-comb_t = Pdf('comb_t', Type = Decay, Observables = [t], Parameters = [bkg_tau],
-             ResolutionModel = tres, Options = ['SingleSided'])
+bkg_tau = RealVar('bkg_tau', Title = 'comb background lifetime', Unit = 'ps', Value = 1, MinMax = (0.0001, 5))
+comb_t = Pdf('comb_t', Type = Decay, Observables = [t], Parameters = [bkg_tau], ResolutionModel = tres, Options = ['SingleSided'])
 
 comb_background = Component('comb_background', { m : bkg_m, mpsi : bkg_mpsi, t : comb_t }, Yield= (50000,0,150000))
 
 
 # Create psi background component
-psi_tau = RealVar('psi_tau', Observable = False, Unit = 'ps', Value = 0.5, MinMax = (0.001, 1))
+psi_tau = RealVar('psi_tau',  Unit = 'ps', Value = 0.5, MinMax = (0.001, 1))
 psi_t = Pdf('psi_t', Type = Decay, Observables = [t], Parameters = [psi_tau], ResolutionModel = tres, Options = ['SingleSided'])
 psi_background = Component('psi_background', { mpsi : sig_mpsi, m : bkg_m, t : comb_t }, Yield= (50000,0,150000) )
 
@@ -84,7 +75,6 @@ pdf = buildPdf((signal, comb_background, psi_background), observables = (m,mpsi)
 
 # Acceptance data
 from ROOT import TFile
-
 f= TFile.Open('Bu2JpsiK_biased_stripping.root')
 assert f
 workspace = f.Get('Bu2JpsiK_workspace')
@@ -105,15 +95,8 @@ splot = RooStats.SPlot('data_splot', 'data_splot', data, pdf._target_(), RooArgL
 from Helpers import Mapping
 mapping = Mapping({m : 'm', mpsi : 'mpsi', t : 'tau'}, data)
 
-ranges = []
-for o in observables:
-    if o.GetName().find('tp') != -1:
-        ranges.append(o)
-
-weights = {}
-for n, o in yields.iteritems():
-    name = o.GetName() + '_sw'
-    weights[n] = observables.find(name)
+ranges = [ 0 for o in observables if o.GetName().find('tp') != -1 ]
+weights = dict( ( n, observables.find(o.GetName() + '_sw') ) for n, o in yields.iteritems() )
 
 ranges.sort()
 range_names = []
@@ -129,13 +112,9 @@ for i in range(0, len(ranges), 2):
 ##     p.setNormRange(norm_range)
 
 from ROOT import TH1F
-histos = {}
-for n in yields.iterkeys():
-    name = 'acceptance_' + n
-    histos[n] = TH1F(name, name, 110, -1.07, 14);
+histos = dict( (n, TH1F('acceptance_' + n, 'acceptance_' + n, 110, -1.07, 14)) for n in yields.iterkeys() )
 
-for i in range(data.numEntries()):
-    data.get(i)
+for evnt in data:
     for n, histo in histos.iteritems():
         b = 1
         interval = 0
@@ -157,7 +136,4 @@ for i in range(data.numEntries()):
 
 from ROOT import TCanvas
 canvas = TCanvas('canvas', 'canvas', 1000, 1000)
-canvas.Divide(2, 2)
-for i, h in enumerate(histos.values()):
-    canvas.cd(i + 1)
-    h.Draw()
+for i, h in zip( canvas.pads(2,2), histos.values() ) : h.Draw()
