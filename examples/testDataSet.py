e6208c9df3513a710961108ec9b5647615660b99
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-11 12:07:54 +0100

    extend testDataSet

--- examples/testDataSet.py
+++ examples/testDataSet.py
@@ -1,6 +1,3 @@
-
-
-
 from RooFitWrappers import *
 ws = RooObject ( workspace = 'myws' )
 
@@ -8,9 +5,16 @@ x = RealVar('x',Value = 1 )
 y = RealVar('y',Value = 2 )
 z = RealVar('z',Value = 3 )
 
-
+l = RooArgList( [ x._var,y._var,z._var ] )
+l.Print()
+s = RooArgSet( [ x._var,y._var,z._var ] )
+s.Print()
 
 a = RooDataSet('foo','foo',[ i._var for i in [ x,y,z] ] )
-print a
 a.Print()
 
+b = RooDataSet('bar','bar',l )
+b.Print()
+
+c = RooDataSet('bar','bar',s )
+c.Print()
