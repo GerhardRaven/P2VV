0c360395c0cb81312f08f836922695319fb5c701
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-17 18:28:05 +0100

    more pure ROOT decorators to dedicated ROOTDecorators.py

--- python/RooFitDecorators.py
+++ python/RooFitDecorators.py
@@ -1,5 +1,6 @@
 from ROOT import RooArgSet, RooArgList, RooDataSet, RooWorkspace, RooFitResult, RooFit
 from ROOT import gStyle,gROOT
+import ROOTDecorators
 gStyle.SetPalette(1)
 gROOT.SetStyle("Plain")
 
@@ -17,35 +18,6 @@ def __wrap_kw_subs( fun ) :
         args += tuple( RooCmdArg( __disp(k,v) ) for k,v in kwargs.iteritems() )
         return fun(self,*args)
     return _fun
-
-###### decorate TPad with pads...
-from ROOT import TPad
-def __pads(self,n=None,m=None) :
-    if n : 
-        if m : self.Divide(n,m)
-        else : self.Divide(n)
-    i=1
-    while self.GetPad(i) :
-        yield self.cd(i) 
-        i += 1
-
-def __frames(self) :
-    for prim in self.GetListOfPrimitives() :
-        if type(prim) == TPad :
-            for prim1 in prim.frames() : yield prim1
-        elif prim.GetName().startswith('TFrame') :
-            yield prim
-
-def __frameHists(self) :
-    for prim in self.GetListOfPrimitives() :
-        if type(prim) == TPad :
-            for prim1 in prim.frameHists() : yield prim1
-        elif prim.GetName().startswith('frame') :
-            yield prim
-
-TPad.pads       = __pads
-TPad.frames     = __frames
-TPad.frameHists = __frameHists
 
 ##########################################
 
