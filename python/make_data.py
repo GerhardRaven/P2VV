8c85eafc5e46dc88c5f25552256ef46d06e258cf
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-22 20:24:09 +0100

    make make_data.py consistent with current code

--- python/make_data.py
+++ python/make_data.py
@@ -1,11 +1,8 @@
 from RooFitWrappers import *
-
 from ROOT import TFile
 
 # setup (singleton) workspace
-from ROOT import RooWorkspace
-ws = RooObject()
-ws.setWorkspace( RooWorkspace("myworkspace") )
+ws = RooObject( workspace = 'myworkspace' )
 
 # now (consistently!) create/declare observables
 m = RealVar('data_m',Observable=True,Unit='MeV/c^2',MinMax=(5000,6000))
@@ -13,46 +10,31 @@ t = RealVar('data_t',Observable=True,MinMax=(-1,14),Unit='ps',Value=0)
 #c = Category('tagdecision',{+1: 'B', -1 : 'Bbar'}, Value='B',  Observable=True )
 #tau = RealVar('sig_tau',Observable=False,Blinded=('UnblindUniform','blindingString', 1.),MinMax=(1,2) )
 
-res_mean = RealVar( 'res_mean',   Observable = False, Unit = 'ps',   Value = 0,  MinMax = ( -10, 10 ) )
-res_sigma = RealVar( 'res_sigma', Observable = False, Unit = '1/ps', Value = 50, MinMax = (  20, 60 ) )
+res_mean = RealVar( 'res_mean',    Unit = 'ps',   Value = 0,  MinMax = ( -10, 10 ) )
+res_sigma = RealVar( 'res_sigma',  Unit = '1/ps', Value = 50, MinMax = (  20, 60 ) )
 
-sig_res = ResolutionModel( 'sig_res', Type = 'RooGaussModel', 
-                           Parameters = [ t, res_mean, res_sigma ] )
-
-sig_tau = RealVar( 'sig_tau', Observable = False, Unit = 'ps', Value = 1.5, MinMax = ( 1., 2. ) )
-
-sig_t = Pdf( 'time', Type = 'Decay', Parameters = ( t, sig_tau, ),
-             Options = ( 'SingleSided', ), ResolutionModel = sig_res )
+sig_res = ResolutionModel( 'sig_res', Type = 'RooGaussModel', Parameters = [ t, res_mean, res_sigma ] )
+sig_tau = RealVar( 'sig_tau',  Unit = 'ps', Value = 1.5, MinMax = ( 1., 2. ) )
+sig_t = Pdf( 'time', Type = 'Decay', Parameters = ( t, sig_tau, sig_res, 'SingleSided') )
 
 mass_mean = RealVar( 'mass_mean',  Unit = 'MeV', Value = 5300, MinMax = ( 5200, 5800 ) )
 mass_sigma = RealVar( 'mass_sigma',  Unit = 'MeV', Value = 15, MinMax = ( 10, 20 ) )
-
 sig_m = Pdf( 'mass', Type = 'Gaussian',  Parameters = ( m, mass_mean, mass_sigma ) )
 
 # create signal and background
-signal = Component('signal')
-signal.setYield(3000,100,6000)
-## signal[m] = 'Gaussian(m,5300,15)'
-signal[m] = sig_m
-## signal[t] = 'Decay(t,sig_tau[1.5,1.0,2.0],TruthModel(t),SingleSided)'
-signal[t] = sig_t
+signal = Component('signal',  Yield=(3000,100,6000) )
+signal += sig_m
+signal += sig_t
 
-background = Component('background')
-background.setYield(3000,1000,6000)
+background = Component('background', Yield = (3000,1000,6000) )
 
 background_c = RealVar( 'background_c',  Unit = '1/MeV', Value = -0.0004)
-background[ m ] = Pdf( 'background',  Type = 'Exponential',
-                       Parameters = ( m, background_c, ) )
+background += Pdf( 'background_m',  Type = 'Exponential', Parameters = ( m, background_c, ) )
 
 background_tau = RealVar( 'background_tau',  Unit = 'ps', Value = 0.4, MinMax = ( 0.1, 0.9 ) )
 background_res = ResolutionModel( 'background_res', Type = 'RooTruthModel', Parameters = [ t ] )
 
-#background[t] = 'Decay(t,bkg_tau[0.4,0.1,0.9],TruthModel(t),SingleSided)'
-background[ t ] = Pdf( 'background', Type = 'Decay', 
-                       Parameters = ( t, background_tau, ), Options = ( 'SingleSided', ),
-                       ResolutionModel = background_res )
-# Exponential(m,-0.004)
-#background[m,t] = 'PROD(Exponential(m,-0.004),Decay(t,bkg_tau[0.4,0.1,0.9],TruthModel(t),SingleSided))'
+background += Pdf( 'background_t', Type = 'Decay', Parameters = ( t, background_tau, background_res , 'SingleSided', ) )
 
 pdf = buildPdf( (background,signal) , Observables = (m,t), Name='pdf' )
 
