19289f94f800840213d639775326cfb41b9f7b92
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-22 21:27:31 +0100

    remove unused code

--- python/TestPdf.py
+++
@@ -1,6 +0,0 @@
-from RooFitWrappers import Pdf
-
-class TestPdf( Pdf ):
-    def __init__( self, name, Observables, ResolutionModel = None ):
-        self._init( name, 'RooAbsPdf' )
-        self.ws
