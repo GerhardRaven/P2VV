5b7a14a58079699938909c843217805b02b2e7d1
Roel Aaij <raaij@nikhef.nl>
2011-12-23 16:30:54 +0100

    Add script to run toys of RooEffHistProd with the signal PDF

--- Tests/toyMC_EffHistProdSig.py
+++ Tests/toyMC_EffHistProdSig.py
@@ -2,14 +2,14 @@ import optparse
 import sys
 parser = optparse.OptionParser(usage = '%prog nbins')
 
-parser.add_option("-f", "--file", dest = "file", default = 'sig.root',
-                  type = 'string', help = "set input filename")
-parser.add_option("-w", "--workspace", dest = "workspace", default = 'w',
-                  type = 'string', help = "set input filename")
-parser.add_option("-p", "--pdf", dest = "pdf", default = 'mc_pdf',
-                  type = 'string', help = "set pdf name")
-parser.add_option("-o", "--observables", dest = "observables", type = 'string',
-                  default = 'trphi,trcostheta,trcospsi,t,tagdecision')
+parser.add_option("-o", "--output", dest = "output", default = 'toy.root',
+                  type = 'string', help = "set output filename")
+parser.add_option("-n", "--ntoys", dest = "ntoys", default = 100,
+                  type = 'int', help = 'number of toys to run')
+parser.add_option("--ncpu", dest = "ncpu", default = 4,
+                  type = 'int', help = 'number of CPUs to use')
+parser.add_option("-e", "--nevents", dest = "nevents", default = 10000,
+                  type = 'int', help = 'number of events to generate')
 
 (options, args) = parser.parse_args()
 if len(args) != 1:
@@ -17,22 +17,94 @@ if len(args) != 1:
     sys.exit(-1)
 nbins = int(args[0])
 
-from ROOT import (gSystem, TH1F, TFile,
-                  RooDataHist, RooFit,
-                  TCanvas)
-gSystem.Load("libP2VV")
-## from math import sqrt,pi
-from RooFitDecorators import *
+from ROOT import RooDataHist
+from RooFitWrappers import *
+from ROOT import TH1F, TFile
 
-root_file = TFile.Open(options.file)
-if not root_file:
-    print 'cannot open root file %s' % options.file
-    sys.exit(-2)
+w = RooObject( workspace = 'w' )
+w = w.ws()
 
-w = root_file.Get(options.workspace)
-if not w:
-    print 'cannot get workspace %s' % options.workspace
-    sys.exit(-2)
+from P2VVParameterizations.AngularFunctions import JpsiphiHelicityAngles as HelAngles, JpsiphiTransversityAngles as TrAngles
+#angles    = HelAngles( cpsi = 'helcthetaK', ctheta = 'helcthetaL', phi = 'helphi' )
+angles = TrAngles( cpsi = 'trcospsi', ctheta = 'trcostheta', phi = 'trphi' )
+t      = RealVar('t', Title = 'decay time', Unit = 'ps', Observable = True, MinMax=(-3,14))
+iTag   = Category('tagdecision' , Title = 'initial state flavour tag', Observable = True, States = { 'B': +1, 'Bbar': -1 } ) # , 'untagged' : 0 } )
+eta    = RealVar('eta', Title = 'estimated mis tag', Observable = False, Constant = True, Value = 0.3, MinMax=(0,0.5) )
+mass   = RealVar('m', Observable = True, Unit = 'MeV/c^2', MinMax = (5200, 5550))
+observables = [ i for i in angles.angles.itervalues() ] + [ t,iTag, mass ]
+
+for i in angles.angles.itervalues() : i.setBins(16)
+t.setBins(48)
+mass.setBins(48)
+mass.setRange('leftsideband', (mass.getMin(),5330) )
+mass.setRange('signal',(5330,5410) )
+mass.setRange('rightsideband',(5410,mass.getMax()) )
+
+from P2VVParameterizations.CPVParams import LambdaSqArg_CPParam
+CP = LambdaSqArg_CPParam( phiCP = { 'Name': 'HelloWorld', 'Value': -0.4, 'MinMax': (-3.2,3.2) }, lambdaCPSq = ConstVar('one',Value=1) )
+
+# polar^2,phase transversity amplitudes, with Apar^2 = 1 - Aperp^2 - A0^2, and delta0 = 0
+from P2VVParameterizations.DecayAmplitudes import JpsiphiAmplitudesLP2011
+amplitudes = JpsiphiAmplitudesLP2011( A0Mag2 = 0.60, A0Phase = 0
+                                    , AperpMag2 = 0.160, AperpPhase = -0.17
+                                    , AparPhase = 2.5
+                                    , ASMag2 = { 'Value' : 0, 'Constant': True} , ASPhase = { 'Value': 0, 'Constant':True } )
+#amplitudes.setConstant('.*AS.*',True)
+
+#### Package from here until the "BTagDecay('name', args)" into a dedicated class/function...
+from P2VVParameterizations.TimePDFs import JpsiphiBTagDecayBasisCoefficients
+# need to specify order in which to traverse...
+basisCoefficients = JpsiphiBTagDecayBasisCoefficients( angles.functions, amplitudes,CP, ['A0','Apar','Aperp','AS'] ) 
+
+#from P2VVParameterizations.TimePDFs import JpsiphiBDecayBasisCoefficients
+#basisCoefficients = JpsiphiBDecayBasisCoefficients( angles.functions, amplitudes,CP, iTag,  ['A0','Apar','Aperp','AS'] ) 
+
+from P2VVParameterizations.FlavourTagging import Trivial_TaggingParams
+taggingParams = Trivial_TaggingParams( wTag = eta ) # FormulaVar('wTag','@2 + @3*(@0-@1)',[eta,etaAverage,p0,p1] ) )
+
+# now build the actual signal PDF...
+from P2VVParameterizations.LifetimeParams import Gamma_LifetimeParams
+lifetimeParams = Gamma_LifetimeParams( Gamma = 0.68, deltaGamma = 0.05, deltaM = dict( Value = 17.8, MinMax = (16,19), Constant = True) )
+
+from P2VVParameterizations.TimeResolution import Truth_TimeResolution
+args = { 'time'      : t
+       , 'iTag'      : iTag
+       , 'dm'        : lifetimeParams['deltaM'] 
+       , 'tau'       : lifetimeParams['MeanLifetime']
+       , 'dGamma'    : lifetimeParams['deltaGamma'] 
+       , 'resolutionModel' : Truth_TimeResolution(time = t)['model']
+       , 'coshCoef'  : basisCoefficients['cosh']
+       , 'cosCoef'   : basisCoefficients['cos']
+       , 'sinhCoef'  : basisCoefficients['sinh']
+       , 'sinCoef'   : basisCoefficients['sin']
+       , 'avgCEven'  : taggingParams['avgCEven'] 
+       , 'avgCOdd'   : taggingParams['avgCOdd']
+       , 'dilution'  : taggingParams['dilution']
+       , 'ADilWTag'  : taggingParams['ADilWTag']
+       }
+
+# TODO: should be able to write BTagDecay('mypdf', **lifetimeParams.BTagDecay() + **basisCoefficients.BTagDecay() + **taggingParams.BTagDecay() )
+
+# update resolution model, and build again...
+from P2VVParameterizations.TimeResolution import LP2011_TimeResolution
+args[ 'resolutionModel' ]  = LP2011_TimeResolution(time = t)['model']
+
+pdf = BTagDecay( 'pdf', **args  )
+
+from P2VVParameterizations.MassPDFs import LP2011_Signal_Mass
+signal = Component('sig', (LP2011_Signal_Mass(mass = mass).pdf(), pdf), Yield = (1000,0,15000))
+
+from P2VVParameterizations.MassPDFs import LP2011_Background_Mass
+from P2VVParameterizations.TimePDFs import LP2011_Background_Time
+from P2VVParameterizations.FlavourTagging import Trivial_Background_Tag
+from P2VVParameterizations.AngularPDFs import Uniform_Angles
+bkg  = Component('bkg',(  LP2011_Background_Mass( mass = mass ).pdf()
+                       ,  LP2011_Background_Time( time = t , resolutionModel = LP2011_TimeResolution(time = t)['model']).pdf()
+                       ,  Trivial_Background_Tag( tagdecision = iTag, bkg_tag_delta = 0.3 ).pdf()
+                       ,  Uniform_Angles( angles = angles.angles ).pdf()
+                       ), Yield = (4000,1000,15000) )
+
+comp_pdf = buildPdf( (signal,bkg), Observables = observables,  Name = 'jointpdf' )
 
 ##############################################
 ### Define acceptance function a la Wouter ###
@@ -50,27 +122,67 @@ _import(effdatahist)
 
 w.factory("HistFunc::eff(t, effdatahist)")
 
-w.factory("EffHistProd::acc_pdf(%s, eff)" % options.pdf )
+w.factory("EffHistProd::acc_pdf(jointpdf, eff)")
 
 w.addClassDeclImportDir("..")
 w.addClassImplImportDir("..")
 w.importClassCode()
 
+# Get the PDF which included the acceptance
 acc_pdf = w.pdf('acc_pdf')
 
-from ROOT import RooFit, RooGenFitStudy
-from ROOT import RooStudyManager
+# Get the bare observable objects and copy them
+obs = w.argSet(','.join([o['Name'] for o in observables]))
+pdf_params = acc_pdf.getParameters(obs)
+gen_params = pdf_params.snapshot(True)
 
-gfs = RooGenFitStudy();
-gfs.setGenConfig("acc_pdf", options.observables, RooFit.NumEvents(1000))
-gfs.setFitConfig("acc_pdf", options.observables, RooFit.Minimizer("Minuit2"))
+# Make another ArgSet to put the fit results in
+result_params = RooArgSet(pdf_params, "result_params")
 
-mgr = RooStudyManager(w, gfs)
+# Get a good random seed, set it and store it
+import struct
+rndm = open('/dev/random','rb')
+seed = struct.unpack('I', rndm.read(4))[0]
 
-## mgr.run(1)
-#mgr.prepareBatchInput('dataSig_%d' % nbins, 100, True)
+from ROOT import RooRandom
+RooRandom.randomGenerator().SetSeed(seed)
 
-## data = gfs.summaryData();
-## root_file = TFile.Open('workspaceSig_%d.root' % nbins, 'recreate')
-## root_file.WriteTObject(w, 'w')
-## root_file.Close()
+# Some extra numbers of interest
+NLL = RooRealVar('NLL', '-log(Likelihood', 1.)
+ngen = RooRealVar('ngen', 'number of generated events', options.nevents)
+seed = RooRealVar('seed', 'random seed', seed)
+result_params.add(NLL)
+result_params.add(ngen)
+result_params.add(seed)
+
+# The dataset to store the results
+result_data = RooDataSet('result_data', 'result_data', result_params)
+
+for i in range(options.ntoys):
+    # Reset pdf parameters to initial values
+    for gen_param in gen_params:
+        pdf_param = pdf_params.find(gen_param.GetName())
+        pdf_param.setVal(gen_param.getVal())
+    data = acc_pdf.generate(obs, options.nevents)
+    fit_result = acc_pdf.fitTo(data, RooFit.NumCPU(options.ncpu), RooFit.Save(True),
+                               RooFit.Minos(False), RooFit.Minimizer('Minuit2'))
+    if fit_result.status() != 0:
+        print 'Fit result status = %s' % fit_result.status()
+        continue
+    NLL.setVal(fit_result.minNll())
+    result_data.fill()
+
+# Write the results to a file
+output_file = TFile.Open(options.output, 'recreate')
+output_file.WriteTObject(result_data, result_data.GetName())
+output_file.Close()
+
+## from ROOT import RooFit, RooGenFitStudy
+## from ROOT import RooStudyManager
+
+## gfs = RooGenFitStudy();
+## obs = ','.join([o.GetName() for o in observables])
+## gfs.setGenConfig("acc_pdf", obs, RooFit.NumEvents(1000))
+## gfs.setFitConfig("acc_pdf", obs, RooFit.Minimizer("Minuit2"))
+
+## mgr = RooStudyManager(w, gfs)
