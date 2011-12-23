93cfcf69f743419860143e019fbc6c60d680a435
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-23 14:22:31 +0100

    regularize TimeResolution & extend mass2.py

--- examples/mass2.py
+++ examples/mass2.py
@@ -2,7 +2,7 @@ from RooFitWrappers import *
 
 ws = RooObject( workspace = 'workspace')
 
-t    = RealVar('time',  Title = 'decay time',    Unit='ps',    Observable = True, MinMax = (-5, 14))
+t    = RealVar('time',  Title = 'decay time',    Unit = 'ps',  Observable = True, MinMax = (1, 14))
 m    = RealVar('mass',  Title = 'M(J/#psi#phi)', Unit = 'MeV', Observable = True, MinMax = (5250, 5450))
 mpsi = RealVar('mdau1', Title = 'M(#mu#mu)',     Unit = 'MeV', Observable = True, MinMax = (3030, 3170))
 mphi = RealVar('mdau2', Title = 'M(KK)',         Unit = 'MeV', Observable = True, MinMax = (1020-8, 1020+8))
@@ -19,33 +19,35 @@ from P2VVParameterizations.MassPDFs import Signal_PsiMass, Background_PsiMass
 sig_mpsi = Signal_PsiMass(     Name = 'sig_mpsi', mass = mpsi )
 bkg_mpsi = Background_PsiMass( Name = 'bkg_mpsi', mass = mpsi )
 
+# phi mass pdf
+
+
 # Decay time pdf
 from P2VVParameterizations.TimeResolution import LP2011_TimeResolution as TimeResolution
-tres = TimeResolution(time = t)['model']
+tres = TimeResolution(time = t)
+tres.setConstant('.*')
 
-from P2VVParameterizations.TimePDFs import Single_Exponent_Time as Signal_Time
-sig_t = Signal_Time( Name = 'sig_t', time = t, resolutionModel = tres, t_sig_tau = dict( Value = 1.5, Name = 'signal_tau', MinMax=(1,2) ) )
+from P2VVParameterizations.TimePDFs import Single_Exponent_Time as Signal_Time, LP2011_Background_Time as Background_Time
+sig_t = Signal_Time(     Name = 'sig_t', time = t, resolutionModel = tres.model(), t_sig_tau = dict( Value = 1.5, Name = 't_sig_tau', MinMax=(1.0,2.0) ) )
+psi_t = Background_Time( Name = 'psi_t', time = t, resolutionModel = tres.model() )
+cmb_t = Background_Time( Name = 'cmb_t', time = t, resolutionModel = tres.model() )
+sig_t.setConstant('.*')
+psi_t.setConstant('.*')
+cmb_t.setConstant('.*')
 
-from ROOT import RooDecay as Decay
-bkg_tau = RealVar('bkg_tau', Title = 'comb background lifetime', Unit = 'ps', Value = 1, MinMax = (0.0001, 5))
-comb_t = Pdf('comb_t', Type = Decay, Parameters = [t, bkg_tau, tres, 'SingleSided'])
-
-# Create psi background component
-psi_tau = RealVar('psi_tau',  Unit = 'ps', Value = 0.5, MinMax = (0.001, 1))
-psi_t = Pdf('psi_t', Type = Decay, Parameters = [t,psi_tau,tres, 'SingleSided'])
 
 # Create components
-ntot = 107182
-nsig = 32020
-fpsi = 0.5
+(ntot,nsig,fpsi) = (107182, 32571, 0.4)
 npsi = (ntot-nsig)*fpsi
 ncmb = (ntot-nsig)*(1-fpsi)
-signal          = Component('signal',          ( sig_m.pdf(),  sig_mpsi.pdf(),  sig_t.pdf() ), Yield = ( nsig, 0.8*nsig, 1.2*nsig) )
-psi_background  = Component('psi_background',  ( bkg_m.pdf(),  sig_mpsi.pdf(),  comb_t ),      Yield=  ( npsi, 0.6*npsi, 1.4*npsi) )
-comb_background = Component('comb_background', ( bkg_m.pdf(),  bkg_mpsi.pdf(),  comb_t ),      Yield = ( ncmb, 0.6*ncmb, 1.4*ncmb) )
+signal         = Component('signal',         ( sig_m.pdf(),  sig_mpsi.pdf(),  sig_t.pdf() ), Yield = ( nsig, 0.8*nsig, 1.2*nsig) )
+psi_background = Component('psi_background', ( bkg_m.pdf(),  sig_mpsi.pdf(),  psi_t.pdf() ), Yield = ( npsi, 0.6*npsi, 1.4*npsi) )
+cmb_background = Component('cmb_background', ( bkg_m.pdf(),  bkg_mpsi.pdf(),  cmb_t.pdf() ), Yield = ( ncmb, 0.6*ncmb, 1.4*ncmb) )
 
 # Build PDF
-pdf = buildPdf((signal, comb_background, psi_background ), Observables = (m,mpsi), Name='pdf')
+pdf  = buildPdf((signal, cmb_background, psi_background ), Observables = (m,mpsi,t), Name='pdf')
+mpdf = buildPdf((signal, cmb_background, psi_background ), Observables = (m,mpsi),   Name='mpdf')
+pdf  = mpdf
 
 from ROOT import TFile
 f = TFile.Open('/data/bfys/dveijk/DataJpsiPhi/2012/Bs2JpsiPhi_ntupleB_for_fitting_20111220.root')
@@ -59,19 +61,19 @@ print 'got dataset with %s candidates' % data.numEntries()
 # Fit
 print 'fitting data'
 from P2VVGeneralUtils import numCPU
+#mpdf.fitTo(data, NumCPU = numCPU() , Timer=1, Extended = True, Verbose = False,  Optimize=0)
+#for p in mpdf.Parameters() : p.setConstant(True)
 pdf.fitTo(data, NumCPU = numCPU() , Timer=1, Extended = True, Verbose = False,  Optimize=0)
 
-from ROOT import kDashed, kRed, kGreen, kBlue
-from ROOT import TCanvas
+from ROOT import TCanvas, kDashed, kRed, kGreen, kBlue, kBlack
 canvas = TCanvas('canvas', 'canvas', 1000, 500)
 obs = pdf.Observables()
 for (p,o) in zip( canvas.pads(len(obs)), obs ) :
-    f = o.frame()
     from P2VVGeneralUtils import plot
     plot( p, o, data, pdf, components = { 'signal*'  : dict( LineColor = kGreen, LineStyle = kDashed )
                                         , 'psi*'     : dict( LineColor = kRed,   LineStyle = kDashed )
-                                        , 'comb*'    : dict( LineColor = kBlue,  LineStyle = kDashed )
+                                        , 'cmb*'     : dict( LineColor = kBlue,  LineStyle = kDashed )
                                         }
-                         , dataOpts = dict( MarkerSize = 0.8, MarkerColor = RooFit.kBlack )
+                         , dataOpts = dict( MarkerSize = 0.8, MarkerColor = kBlack )
                          , pdfOpts  = dict( LineWidth = 2 )
                          )
--- python/P2VVParameterizations/TimeResolution.py
+++ python/P2VVParameterizations/TimeResolution.py
@@ -12,6 +12,7 @@ from P2VVParameterizations.GeneralUtils import _util_parse_mixin
 class TimeResolution ( _util_parse_mixin ) :
     def __init__( self, model ) : self._model = model
     def __getitem__( self, kw ) : return getattr( self, '_' + kw )
+    def model( self ) : return self._model
 
 
 class Truth_TimeResolution ( TimeResolution ) :
@@ -20,9 +21,9 @@ class Truth_TimeResolution ( TimeResolution ) :
         self._parseArg( 'time',         kwargs, Title = 'Decay time', Unit = 'ps', Observable = True, Value = 0., MinMax = ( -0.5, 5. ) )
 
         from ROOT import RooTruthModel as TruthModel
-        TimeResolution.__init__( self, ResolutionModel(  'timeResModelTruth'
-                                                       , Type = TruthModel
-                                                       , Parameters = [ self._time ] )
+        TimeResolution.__init__( self, ResolutionModel( 'timeResModelTruth'
+                                                      , Type = TruthModel
+                                                      , Parameters = [ self._time ] )
                                )
 
 class Gaussian_TimeResolution ( TimeResolution ) :
@@ -33,17 +34,17 @@ class Gaussian_TimeResolution ( TimeResolution ) :
         self._parseArg( 'timeResSigma', kwargs, Title = 'Decay time resolution width', Value = 0.05 )
 
         from ROOT import RooGaussModel as GaussModel
-        TimeResolution.__init__( self, ResolutionModel(  'timeResModelGauss'
-                                                       , Type = GaussModel
-                                                       , Parameters  = [ self._time, self._timeResMu, self._timeResSigma ] )
+        TimeResolution.__init__( self, ResolutionModel( 'timeResModelGauss'
+                                                      , Type = GaussModel
+                                                      , Parameters  = [ self._time, self._timeResMu, self._timeResSigma ] )
                                )
 
 class LP2011_TimeResolution ( TimeResolution ) :
     def __init__( self, **kwargs ) :
         from RooFitWrappers import ResolutionModel, AddModel, ConstVar, RealVar
-        self._parseArg( 'time', kwargs, Title = 'Decay time', Unit = 'ps', Observable = True, Value = 0., MinMax = ( -0.5, 5. ) )
-        self._timeResMu = ConstVar( 'timeResMu', Value = -0.0027 )
-        self._timeResSF = RealVar(  'timeResSF', Value = 1.0, MinMax = ( 0.5, 5. ) )
+        self._parseArg( 'time',      kwargs, Title = 'Decay time', Unit = 'ps', Observable = True, Value = 0., MinMax = ( -0.5, 5. ) )
+        self._parseArg( 'timeResMu', kwargs, Value = -0.0027, Constant = True )
+        self._parseArg( 'timeResSF', kwargs, Value = 1.0, MinMax = ( 0.5, 5. ) )
 
         sigmas = [ ( 3, 0.513  ), ( 2, 0.0853 ), ( 1, 0.0434 ) ]
         fracs  = [ ( 3, 0.0017 ), ( 2, 0.165 ) ]
@@ -51,12 +52,12 @@ class LP2011_TimeResolution ( TimeResolution ) :
         self._timeResFracs  = [ ConstVar( 'timeResFrac%s'  % num, Value = val ) for num, val in fracs  ]
 
         from ROOT import RooGaussModel as GaussModel
-        TimeResolution.__init__( self, AddModel(  'timeResModelLP2011'
-                                                , [ ResolutionModel(  'timeResLP2011_%s' % numVal[0]
-                                                                    , Type = GaussModel
-                                                                    , Parameters  = [ self._time, self._timeResMu, sigma, self._timeResSF ]
-                                                                   )\
-                                                    for ( numVal, sigma ) in zip( sigmas, self._timeResSigmas ) ]
-                                                , self._timeResFracs )
+        TimeResolution.__init__( self, AddModel( 'timeResModelLP2011'
+                                               , [ ResolutionModel( 'timeResLP2011_%s' % numVal[0]
+                                                                  , Type = GaussModel
+                                                                  , Parameters  = [ self._time, self._timeResMu, sigma, self._timeResSF ]
+                                                                  )\
+                                                   for ( numVal, sigma ) in zip( sigmas, self._timeResSigmas ) ]
+                                               , self._timeResFracs )
                                )
 
