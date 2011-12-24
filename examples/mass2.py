1039876e3c7122b3a60b30881cd404ec19a63208
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2011-12-24 12:26:34 +0100

    add generic angle PDF to mass2

--- examples/mass2.py
+++ examples/mass2.py
@@ -5,6 +5,7 @@ t    = RealVar('time',  Title = 'decay time',    Unit = 'ps',  Observable = True
 m    = RealVar('mass',  Title = 'M(J/#psi#phi)', Unit = 'MeV', Observable = True, MinMax = (5259, 5451), nBins = 48 )
 mpsi = RealVar('mdau1', Title = 'M(#mu#mu)',     Unit = 'MeV', Observable = True, MinMax = (3025, 3169), nBins = 32 )
 mphi = RealVar('mdau2', Title = 'M(KK)',         Unit = 'MeV', Observable = True, MinMax = (1020-8, 1020+8) )
+iTag = Category( 'tagdecision' , Title = 'initial state flavour tag',   Observable = True,  States = { 'B': +1, 'Bbar': -1, 'untagged' : 0 } )
 from P2VVParameterizations.AngularFunctions import JpsiphiHelicityAngles as HelAngles, JpsiphiTransversityAngles as TrAngles
 #angles    = HelAngles( cpsi = 'helcthetaK', ctheta = 'helcthetaL', phi = 'helphi' )
 angles    = TrAngles( cpsi   = dict( Name = 'trcospsi',   Title = 'cos(#psi)',        nBins = 24 )
@@ -12,7 +13,7 @@ angles    = TrAngles( cpsi   = dict( Name = 'trcospsi',   Title = 'cos(#psi)',  
                     , phi    = dict( Name = 'trphi',      Title = '#phi_{tr}',        nBins = 24 ) 
                     )
 
-observables = [ mpsi,mphi,m,t,angles.angles['cpsi'],angles.angles['ctheta'],angles.angles['phi'] ]
+observables = [ iTag, mpsi,mphi,m,t,angles.angles['cpsi'],angles.angles['ctheta'],angles.angles['phi'] ]
 
 #unbiased data only: /data/bfys/dveijk/DataJpsiPhi/2012/Bs2JpsiPhi_DTT_after_yuehongs_script_20111220.root
 from P2VVGeneralUtils import readData
@@ -36,27 +37,27 @@ bkg_mpsi = Background_PsiMass( Name = 'bkg_mpsi', mass = mpsi )
 
 # phi mass pdf
 
-# angle pdfs
-#from P2VVParameterizations.AngularPDFs import AngleBasis_AngularPdfTerms
-#indices  = [ ( PIndex, YIndex0, YIndex1 ) for PIndex in range(3) for YIndex0 in range(3) for YIndex1 in range( -YIndex0, YIndex0 + 1 ) ]
-#cnvrtInd = lambda ind : 'm' + str(abs(ind)) if ind < 0 else str(ind)
-#sig_angles = AngleBasis_AngularPdfTerms(  Angles = angles.angles
-#                                          , **dict( (  'C%d%d%s' % ( inds[0], inds[1], cnvrtInd(inds[2]) )
-#                                                     , {  'Name'    : 'C_sig_ab%d%d%s' % ( inds[0], inds[1], cnvrtInd(inds[2]) )
-#                                                        , 'Value'   : 1.
-#                                                        , 'MinMax'  : ( -1., +1. )
-#                                                        , 'Indices' : inds
-#                                                       }
-#                                                    ) for inds in indices
-#                                                  )
-#                                         )
-#mcpdf = coefPDFTerms.buildSumPdf('angCoefsPDF')
-from P2VVParameterizations.AngularPDFs import Uniform_Angles
-all_angles =  Uniform_Angles( angles = angles.angles )
+# angle pdfs (background only)
+from P2VVParameterizations.AngularPDFs import AngleBasis_AngularPdfTerms
+indices = lambda i,l : ( ( _i, _l, _m ) for _i in range(i) for _l in range(l) for _m in range( -_l, _l + 1 )  )
+coefPDFTerms = AngleBasis_AngularPdfTerms(  Angles = angles.angles
+                                          , **dict( (  ('angles_bkg_f_%d%d%d' % i ).replace('-','m')
+                                                     , {  'Name'     : 'angles_bkg_%d%d%d' % i ).replace('-','m')
+                                                        , 'Value'    : 1. if i == (0,0,0) else 0.
+                                                        , 'MinMax'   : ( 0.99, 1.01 )  if i == (0,0,0) else ( -0.2,+0.2 ) if i[1]==0 else (-0.1,0.1)
+                                                        , 'Indices'  : i
+                                                        , 'Constant' : i == (0,0,0)
+                                                       }
+                                                    ) for i in indices(4,3)
+                                                  )
+                                         )
+bkg_angles = coefPDFTerms.buildSumPdf('angCoefsPDF')
+#from P2VVParameterizations.AngularPDFs import Uniform_Angles
+#bkg_angles =  Uniform_Angles( angles = angles.angles )
 
 # Decay time pdf
 from P2VVParameterizations.TimeResolution import LP2011_TimeResolution as TimeResolution
-tres = TimeResolution(time = t)
+tres = TimeResolution(time = t) # TODO: extend _util_parse_mixin so that we can add: , Constant = '.*')
 tres.setConstant('.*')
 
 from P2VVParameterizations.TimePDFs import Single_Exponent_Time as Signal_Time, LP2011_Background_Time as Background_Time
@@ -64,13 +65,18 @@ sig_t = Signal_Time(     Name = 'sig_t', time = t, resolutionModel = tres.model(
 psi_t = Background_Time( Name = 'psi_t', time = t, resolutionModel = tres.model() )
 cmb_t = Background_Time( Name = 'cmb_t', time = t, resolutionModel = tres.model() )
 
+
+# itag distribution (background only)
+from P2VVParameterizations.FlavourTagging import Trivial_Background_Tag
+bkg_tag = Trivial_Background_Tag( tagdecision = iTag, bkg_tag_delta = 0.0 )
+
 # Create components
 (ntot,nsig,fpsi) = (data.numEntries(), 20000, 0.4)
 npsi = (ntot-nsig)*fpsi
 ncmb = (ntot-nsig)*(1-fpsi)
-signal         = Component('signal',         ( sig_m.pdf(),  sig_mpsi.pdf(),  sig_t.pdf(), all_angles.pdf() ), Yield = ( nsig, 0.8*nsig, 1.2*nsig) )
-psi_background = Component('psi_background', ( psi_m.pdf(),  sig_mpsi.pdf(),  psi_t.pdf(), all_angles.pdf() ), Yield = ( npsi, 0.6*npsi, 1.4*npsi) )
-cmb_background = Component('cmb_background', ( cmb_m.pdf(),  bkg_mpsi.pdf(),  cmb_t.pdf(), all_angles.pdf() ), Yield = ( ncmb, 0.6*ncmb, 1.4*ncmb) )
+signal         = Component('signal',         ( sig_m.pdf(),  sig_mpsi.pdf(),  sig_t.pdf(), bkg_angles ), Yield = ( nsig, 0.8*nsig, 1.2*nsig) )
+psi_background = Component('psi_background', ( psi_m.pdf(),  sig_mpsi.pdf(),  psi_t.pdf(), bkg_angles ), Yield = ( npsi, 0.6*npsi, 1.4*npsi) )
+cmb_background = Component('cmb_background', ( cmb_m.pdf(),  bkg_mpsi.pdf(),  cmb_t.pdf(), bkg_angles ), Yield = ( ncmb, 0.6*ncmb, 1.4*ncmb) )
 
 # Build PDF
 pdf  = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi,t)+tuple(angles.angles.itervalues()), Name='pdf')
