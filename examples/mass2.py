9a3df283a54e8aec286eed6b7ff9291d7ed7d576
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2012-01-02 20:36:03 +0100

    do unbiased, tagged fit without tagging calibration, without angular efficiency

--- examples/mass2.py
+++ examples/mass2.py
@@ -5,7 +5,11 @@ obj  = RooObject( workspace = 'workspace')
 # define observables
 t    = RealVar('time',  Title = 'decay time',    Unit = 'ps',  Observable = True, MinMax = (0.5, 14),    nBins =  54 )
 st   = RealVar('sigmat',Title = '#sigma(t)',     Unit = 'ps',  Observable = True, MinMax = (0.0, 0.15),  nBins =  50 )
-m    = RealVar('mass',  Title = 'M(J/#psi#phi)', Unit = 'MeV', Observable = True, MinMax = (5259, 5451), nBins =  48 )
+m    = RealVar('mass',  Title = 'M(J/#psi#phi)', Unit = 'MeV', Observable = True, MinMax = (5259, 5451), nBins =  48
+                     ,  Ranges =  { 'leftsideband'  : ( None, 5330 )
+                                  , 'signal'        : ( 5330, 5410 )
+                                  , 'rightsideband' : ( 5410, None ) 
+                                  } )
 mpsi = RealVar('mdau1', Title = 'M(#mu#mu)',     Unit = 'MeV', Observable = True, MinMax = (3025, 3169), nBins =  32 )
 mphi = RealVar('mdau2', Title = 'M(KK)',         Unit = 'MeV', Observable = True, MinMax = (1012, 1028), nBins =  16 )
 iTag = Category( 'tagdecision_os', Title = 'initial state flavour tag', Observable = True, States = { 'B': +1, 'Bbar': -1, 'untagged' : 0 } )
@@ -21,24 +25,14 @@ observables = [ iTag,eta, mpsi,mphi,m,t,angles.angles['cpsi'],angles.angles['cth
 
 #unbiased data only: /data/bfys/dveijk/DataJpsiPhi/2012/Bs2JpsiPhi_DTT_after_yuehongs_script_20111220.root
 from P2VVGeneralUtils import readData
-#data = readData( '/tmp/Bs2JpsiPhi_ntupleB_for_fitting_20111220.root'
-data = readData( '/data/bfys/dveijk/DataJpsiPhi/2012/Bs2JpsiPhi_ntupleB_for_fitting_20111220.root'
+#data = readData( '/data/bfys/dveijk/DataJpsiPhi/2012/Bs2JpsiPhi_ntupleB_for_fitting_20111220.root'
+data = readData( '/tmp/Bs2JpsiPhi_ntupleB_for_fitting_20111220.root'
                , 'DecayTree'
                , True
                , observables
                )
-obj.ws().put(data)
-Di  = RealVar('tagdilution', Title = 'estimated dilution', Observable = True, Formula = ( "@0*(1-2*@1)",[iTag,eta], data ), MinMax=(-1,1),nBins =  51)
-
-
-# TODO: add a wrapper that allows one to pass ranges as dict: m.setRanges( { 'leftsideband' : (m.getMin(),5330), 'signal' : (5330,5410), ... } )
-#       even more fance: allow None in the upper or lower limit to indicate it should extend to getMax resp. getMin...
-#       m.setRanges( { 'leftsideband'  : ( None, 5330 )
-#                    , 'signal'        : ( 5330, 5410 )
-#                    , 'rightsideband' : ( 5410, None ) } )
-m.setRange('leftsideband', (m.getMin(),5330) )
-m.setRange('signal',(5330,5410) )
-m.setRange('rightsideband',(5410,m.getMax()) )
+tag  = RealVar('tagdilution', Title = 'estimated dilution', Observable = True, Formula = ( "@0*(1-2*@1)",[iTag,eta], data ), MinMax=(-1,1),nBins =  51)
+observables = [ tag, mpsi,mphi,m,t,angles.angles['cpsi'],angles.angles['ctheta'],angles.angles['phi'], st ]
 
 # B mass pdf
 from P2VVParameterizations.MassPDFs import LP2011_Signal_Mass as Signal_BMass, LP2011_Background_Mass as Background_BMass
@@ -54,8 +48,8 @@ bkg_mpsi = Background_PsiMass( Name = 'bkg_mpsi', mass = mpsi )
 # sigma(t) pdf
 from P2VVParameterizations.TimeResolution import Gamma_Sigmat
 sig_st = Gamma_Sigmat( Name = 'sig_st', st = st, st_sig_gamma = dict( Name = 'st_sig_gamma', Value = 12. ), st_sig_beta = dict( Name = 'st_sig_beta', Value = 0.003 ) )
-cmb_st = Gamma_Sigmat( Name = 'cmb_st', st = st, st_sig_gamma = dict( Name = 'st_cmb_gamma', Value = 9.6 ), st_sig_beta = dict( Name = 'st_cmb_beta', Value = 0.004 ) )
 psi_st = Gamma_Sigmat( Name = 'psi_st', st = st, st_sig_gamma = dict( Name = 'st_psi_gamma', Value = 5.5 ), st_sig_beta = dict( Name = 'st_psi_beta', Value = 0.008 ) )
+cmb_st = Gamma_Sigmat( Name = 'cmb_st', st = st, st_sig_gamma = dict( Name = 'st_cmb_gamma', Value = 9.6 ), st_sig_beta = dict( Name = 'st_cmb_beta', Value = 0.004 ) )
 
 # phi mass pdf
 
@@ -70,15 +64,12 @@ tres.setConstant('.*')
 from P2VVParameterizations.LifetimeParams import Gamma_LifetimeParams
 lifetimeParams = Gamma_LifetimeParams( Gamma = 0.68, deltaGamma = 0.05, deltaM = dict( Value = 17.8, MinMax = (16,19), Constant = True) )
 
-from P2VVParameterizations.FlavourTagging import Trivial_TaggingParams
-#taggingParams = Trivial_TaggingParams( wTag = eta ) # FormulaVar('wTag','@2 + @3*(@0-@1)',[eta,etaAverage,p0,p1] ) )
-#taggingParams = Trivial_TaggingParams( wTag = ConstVar( Name = 'half', Value = 0.5 ) ) # FormulaVar('wTag','@2 + @3*(@0-@1)',[eta,etaAverage,p0,p1] ) )
 from P2VVParameterizations.FlavourTagging import Trivial_Dilution
-taggingParams = Trivial_Dilution( Dilution = Di ) # TODO: add calibration...
+taggingParams = Trivial_Dilution( Dilution = tag ) # TODO: add calibration...
 
 from math import pi
 from P2VVParameterizations.CPVParams import LambdaSqArg_CPParam
-CP = LambdaSqArg_CPParam( phiCP      = dict( Name = 'phi_s', Value = -0.04, MinMax = (-pi,pi), Constant = True )
+CP = LambdaSqArg_CPParam( phiCP      = dict( Name = 'phi_s', Value = -0.04, MinMax = (-pi,pi), Constant = False )
                         , lambdaCPSq = ConstVar( Name = 'one', Value = 1 ) 
                         )
 
@@ -93,7 +84,7 @@ amplitudes = JpsiphiAmplitudesLP2011( A0Mag2 = 0.60, A0Phase = 0
 
 # need to specify order in which to traverse...
 from P2VVParameterizations.TimePDFs import JpsiphiBDecayBasisCoefficients
-basisCoefficients = JpsiphiBDecayBasisCoefficients( angles.functions, amplitudes,CP, Di, ['A0','Apar','Aperp','AS'] ) 
+basisCoefficients = JpsiphiBDecayBasisCoefficients( angles.functions, amplitudes,CP, tag, ['A0','Apar','Aperp','AS'] ) 
 
 # TODO: should be able to write BTagDecay('mypdf', **lifetimeParams.BTagDecay() + **basisCoefficients.BTagDecay() + **taggingParams.BTagDecay() )
 # TODO: unify keys left and right....
@@ -108,7 +99,7 @@ sig_t_angles = BDecay( Name      = 'sig_t_angles'
                             , cosCoef   = basisCoefficients['cos']
                             , sinhCoef  = basisCoefficients['sinh']
                             , sinCoef   = basisCoefficients['sin']
-                            , ConditionalObservables = ( Di, )
+                            , ConditionalObservables = ( tag, )
                             )
 
 from P2VVParameterizations.TimePDFs import Single_Exponent_Time as Signal_Time, LP2011_Background_Time as Background_Time
@@ -117,18 +108,18 @@ psi_t = Background_Time( Name = 'psi_t', time = t, resolutionModel = tres.model(
 cmb_t = Background_Time( Name = 'cmb_t', time = t, resolutionModel = tres.model(), t_bkg_fll = dict( Name = 't_cmb_fll'), t_bkg_ll_tau = dict( Name = 't_cmb_ll_tau'), t_bkg_ml_tau = dict( Name = 't_cmb_ml_tau') )
 
 # itag distribution (background only)
-from P2VVParameterizations.FlavourTagging import Trivial_Background_Tag
-bkg_tag = Trivial_Background_Tag( Name = 'bkg_tag', tagdecision = iTag, bkg_tag_eps = dict( Name = 'tag_bkg_eff', Value = 0.35 ), bkg_tag_delta = dict( Name = 'tag_bkg_delta', Value = 0 ) )
-sig_tag = Trivial_Background_Tag( Name = 'sig_tag', tagdecision = iTag, bkg_tag_eps = dict( Name = 'tag_sig_eff', Value = 0.35 ), bkg_tag_delta = dict( Name = 'tag_sig_delta', Value = 0 ) )
+#from P2VVParameterizations.FlavourTagging import Trivial_Background_Tag
+#bkg_tag = Trivial_Background_Tag( Name = 'bkg_tag', tagdecision = iTag, bkg_tag_eps = dict( Name = 'tag_bkg_eff', Value = 0.35 ), bkg_tag_delta = dict( Name = 'tag_bkg_delta', Value = 0 ) )
+#sig_tag = Trivial_Background_Tag( Name = 'sig_tag', tagdecision = iTag, bkg_tag_eps = dict( Name = 'tag_sig_eff', Value = 0.35 ), bkg_tag_delta = dict( Name = 'tag_sig_delta', Value = 0 ) )
 
 
 # Create components
 (ntot,nsig,fpsi) = (data.numEntries(), 23000, 0.43)
 npsi = (ntot-nsig)*fpsi
 ncmb = (ntot-nsig)*(1-fpsi)
-signal         = Component('signal',         ( sig_m.pdf(),  sig_mpsi.pdf(), sig_st.pdf(), sig_t_angles, sig_tag.pdf() ), Yield = ( nsig, 0.9*nsig, 1.1*nsig) )
-psi_background = Component('psi_background', ( psi_m.pdf(),  sig_mpsi.pdf(), psi_st.pdf(), psi_t.pdf(),  bkg_tag.pdf() ), Yield = ( npsi, 0.7*npsi, 1.3*npsi) )
-cmb_background = Component('cmb_background', ( cmb_m.pdf(),  bkg_mpsi.pdf(), cmb_st.pdf(), cmb_t.pdf(),  bkg_tag.pdf() ), Yield = ( ncmb, 0.7*ncmb, 1.3*ncmb) )
+signal         = Component('signal',         ( sig_m.pdf(),  sig_mpsi.pdf(), sig_st.pdf(), sig_t_angles, ), Yield = ( nsig, 0.9*nsig, 1.1*nsig) )
+psi_background = Component('psi_background', ( psi_m.pdf(),  sig_mpsi.pdf(), psi_st.pdf(), psi_t.pdf(),  ), Yield = ( npsi, 0.7*npsi, 1.3*npsi) )
+cmb_background = Component('cmb_background', ( cmb_m.pdf(),  bkg_mpsi.pdf(), cmb_st.pdf(), cmb_t.pdf(),  ), Yield = ( ncmb, 0.7*ncmb, 1.3*ncmb) )
 
 
 def FitAndPlot( pdf, data, fitOpts = dict(), sdata = None ) :
@@ -164,34 +155,36 @@ def FitAndPlot( pdf, data, fitOpts = dict(), sdata = None ) :
                                  , pdfOpts  = dict( LineWidth = 2, **pdfRng )
                                  , logy = ( o == t )
                                  )
-    # TODO: move this bit to a dedicated function...
-    # sPlot if sdata. Only do this for observables NOT used to create the sData
-    if sdata :
-        canvas['splot'] = TCanvas('splot')
-        obs = observables
-        obs = [ o for o in obs if o in pdf.Observables() ]
-        obs = [ o for o in obs if hasattr(o,'frame') ]
-        obs = [ o for o in obs if o not in sdata.usedObservables() ]
-        for (p,o) in zip( canvas['splot'].pads(len(obs)), obs ) :
-            # snapshot yeilds
-            _yields = dict( (p,p.getVal()) for p in pdf.Parameters() if p.getAttribute('Yield')  )
-            # loop over components
-            for (pp,i) in zip( p.pads(1,len(_yields)), _yields.iterkeys() ) :
-                # switch off all yields, except current one
-                for j in _yields.iterkeys() :   
-                    if i!=j :   j.setVal(0)
-                # and plot both weighed data and PDF
-                from P2VVGeneralUtils import plot
-                # TODO: add the same color coding as above...
-                c_name = i.GetName()[2:]
-                c_opts = { 'signal'   : dict( LineColor = kGreen )
-                         , 'psi_background'     : dict( LineColor = kRed )
-                         , 'cmb_background'     : dict( LineColor = kBlue )
-                         }
-                plot( pp, o, sdata.data( c_name ), pdf, pdfOpts = c_opts[c_name] if c_name in c_opts else {})
-                # and put back the original value!
-                for (i,v) in _yields.iteritems() : i.setVal(v)
     return (result,canvas)
+
+def splot( pdf, sdata ) :
+    from ROOT import TCanvas, kDashed, kRed, kGreen, kBlue, kBlack
+    canvas = dict()
+    canvas['splot'] = TCanvas('splot')
+    obs = observables
+    obs = [ o for o in obs if o in pdf.Observables() ]
+    obs = [ o for o in obs if hasattr(o,'frame') ]
+    obs = [ o for o in obs if o not in sdata.usedObservables() ]
+    for (p,o) in zip( canvas['splot'].pads(len(obs)), obs ) :
+        # snapshot yeilds
+        _yields = dict( (p,p.getVal()) for p in pdf.Parameters() if p.getAttribute('Yield')  )
+        # loop over components
+        for (pp,i) in zip( p.pads(1,len(_yields)), _yields.iterkeys() ) :
+            # switch off all yields, except current one
+            for j in _yields.iterkeys() :   
+                if i!=j :   j.setVal(0)
+            # and plot both weighed data and PDF
+            from P2VVGeneralUtils import plot
+            # TODO: add the same color coding as above...
+            c_name = i.GetName()[2:]
+            c_opts = { 'signal'             : dict( LineColor = kGreen )
+                     , 'psi_background'     : dict( LineColor = kRed )
+                     , 'cmb_background'     : dict( LineColor = kBlue )
+                     }
+            plot( pp, o, sdata.data( c_name ), pdf, pdfOpts = c_opts[c_name] if c_name in c_opts else {})
+            # and put back the original value!
+            for (i,v) in _yields.iteritems() : i.setVal(v)
+    return canvas
 
 # Build, Fit and Plot PDFs
 pdf_m = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi), Name='pdf_m')
@@ -200,16 +193,13 @@ for p in pdf_m.Parameters() : p.setConstant( not p.getAttribute('Yield') )
 from P2VVGeneralUtils import SData
 splot_m = SData(Pdf = pdf_m, Data = data, Name = 'MassSplot')
 
-# get histograms for eta...
+# get histograms for dilution...
 from RooFitWrappers import HistPdf
 for c in [ signal, psi_background,cmb_background ] :
-    # Must remove the spike of the untagged events!!!
-    tmpdata = splot_m.data( c.GetName() )
+    c += HistPdf(Name = "dilution_%s_pdf"%c.GetName(), Observables = [ tag ], Data = splot_m.data(c.GetName() ) )
 
-    c += HistPdf(Name = "eta_%s_pdf"%c.GetName(), Observables = [ eta ], Data = splot_m.data(c.GetName() ) )
-
-
-#signal.setConditional( [ eta, tagdecision ], True )
+pdf_di = buildPdf( (signal, cmb_background, psi_background), Observables = (tag,), Name = 'pdf_di')
+c_di = splot( pdf_di, splot_m)
 
 from P2VVParameterizations.AngularPDFs import SPlot_Moment_Angles
 mompdfBuilder = SPlot_Moment_Angles( angles.angles , splot_m )
@@ -217,11 +207,11 @@ mompdfBuilder = SPlot_Moment_Angles( angles.angles , splot_m )
 psi_background += mompdfBuilder.pdf( Component = psi_background.GetName(), Indices = [ i for i in indices(2,3) ], Name = 'psi_angles' )
 cmb_background += mompdfBuilder.pdf( Component = cmb_background.GetName(), Indices = [ i for i in indices(3,4) ], Name = 'cmb_angles' )
 
-pdf_mst = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi,st), Name='pdf_mst')
-c_mst = FitAndPlot(pdf_mst,data,sdata = splot_m)
-for p in pdf_mst.Parameters() : p.setConstant(True)
+#pdf_mst = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi,st), Name='pdf_mst')
+#c_mst = FitAndPlot(pdf_mst,data,sdata = splot_m)
+#for p in pdf_mst.Parameters() : p.setConstant(True)
 
 # full fit
-pdf  = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi,t,iTag,eta)+tuple(angles.angles.itervalues()), Name='pdf')
+pdf  = buildPdf((signal, cmb_background, psi_background), Observables = (m,mpsi,t,tag)+tuple(angles.angles.itervalues()), Name='pdf')
 c_pdf = FitAndPlot(pdf,data, sdata = splot_m)
 
