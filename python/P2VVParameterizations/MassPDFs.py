333d666c4b7d7abfefb413a48aa3ced73031ddde
Jeroen van Leerdam <jleerdam@nikhef.nl>
2012-07-04 15:25:45 +0200

    float signal B mass yield and width fractions in fit

--- python/P2VVParameterizations/MassPDFs.py
+++ python/P2VVParameterizations/MassPDFs.py
@@ -64,24 +64,24 @@ class Binned_MassPdf( MassPdf ) :
 
 class LP2011_Signal_Mass ( MassPdf ) :
     def __init__(self, mass, **kwargs ) :
-        self._parseArg('m_sig_mean', kwargs, Title = 'B Mass', Unit = 'MeV/c^2', Value = 5368., MinMax = (5360.,5372.) )
-        self._parseArg('m_sig_sigma_1', kwargs, Title = 'B Mass resolution 1', Unit = 'MeV/c^2', Value = 6.45, MinMax = (3.,20.) )
-        self._parseArg('m_sig_sigma_2_scale', kwargs, Title = 'B Mass resolution 2 scale factor', Unit = 'MeV/c^2', Value = 2.258, MinMax = (1,5), Constant = True )
-        self._parseArg('m_sig_f',kwargs, Title = 'B mass fraction 2nd Gaussian', Value = 0.803, MinMax = (0.,1.), Constant = True )
+        self._parseArg( 'm_sig_mean',     kwargs, Title = 'B Mass', Value = 5368., MinMax = ( 5360., 5372. ), Unit = 'MeV/c^2' )
+        self._parseArg( 'm_sig_sigma_1',  kwargs, Title = 'B Mass resolution 1', Value = 6.3, MinMax = (3., 20.), Unit = 'MeV/c^2' )
+        self._parseArg( 'm_sig_sigma_sf', kwargs, Title = 'B Mass resolution 2:1 scale factor', Value = 2.258, MinMax = ( 1., 5. ) )
+        self._parseArg( 'm_sig_frac',     kwargs, Title = 'B mass fraction 2nd Gaussian', Value = 0.803, MinMax = ( 0., 2. ) )
 
         from ROOT import RooGaussian as Gaussian
         from RooFitWrappers import Pdf, FormulaVar, SumPdf
-        g1 = Pdf( Name ='m_sig_1'
-                , Type = Gaussian
-                , Parameters = (mass, self._m_sig_mean, self._m_sig_sigma_1 )
+        g1 = Pdf(  Name ='m_sig_1'
+                 , Type = Gaussian
+                 , Parameters = (mass, self._m_sig_mean, self._m_sig_sigma_1 )
                 )
         g2 = Pdf( Name = 'm_sig_2'
-                , Type = Gaussian
-                , Parameters = ( mass, self._m_sig_mean
-                               , FormulaVar('m_sig_sigma_2','@0*@1',(self._m_sig_sigma_2_scale, self._m_sig_sigma_1))
-                               )
+                 , Type = Gaussian
+                 , Parameters = ( mass, self._m_sig_mean
+                                 , FormulaVar( 'm_sig_sigma_2', '@0*@1', ( self._m_sig_sigma_sf, self._m_sig_sigma_1 ) ) )
                 )
-        MassPdf.__init__(self, pdf = SumPdf(Name = kwargs.pop('Name','LP2011_Signal_Mass'),   PDFs = (  g1,  g2)  , Yields = { g1.GetName() : self._m_sig_f } ) )
+        MassPdf.__init__( self, pdf = SumPdf( Name = kwargs.pop( 'Name', 'LP2011_Signal_Mass' ), PDFs = ( g1, g2 )
+                         , Yields = { g1.GetName() : self._m_sig_frac } ) )
 
 
 class LP2011_Background_Mass ( MassPdf ) :
