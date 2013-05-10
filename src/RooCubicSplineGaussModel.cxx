0986f3df21e5336f8f4ac4a8fb0852cacb45bf90
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2013-05-10 11:14:05 +0200

    refactor RooCubicSplineGaussModel, RooCubicSplineFun such that only RooCubicSplineFun knows about RooCubicSplineKnot

--- src/RooCubicSplineGaussModel.cxx
+++ src/RooCubicSplineGaussModel.cxx
@@ -30,7 +30,7 @@
 #include "TMath.h"
 #include "Riostream.h"
 #include "P2VV/RooCubicSplineGaussModel.h"
-#include "P2VV/RooCubicSplineKnot.h"
+#include "P2VV/RooCubicSplineFun.h"
 #include "RooMath.h"
 #include "RooComplex.h"
 #include "RooRealConstant.h"
@@ -60,8 +60,6 @@ namespace {
       return v.exp()*(zsq.exp()/(iz*rootpi) + 1)*2 ;
     }
 
-
-
     // Calculate Re[exp(-x^2) cwerf(i (z-x) )], taking care of numerical instabilities
     Double_t evalRe(Double_t x, const RooComplex& z) {
       Double_t re =  z.re()-x;
@@ -82,33 +80,6 @@ namespace {
       return (re>-5.0) ? RooMath::FastComplexErrFunc(RooComplex(-z.im(),re))*exp(-x*x) 
                        : evalApprox(x,z) ;
     }
-
-    class K_n {
-    public:
-        K_n(const RooComplex& z) : _zi( RooComplex(1,0)/z) {}
-        RooComplex operator()(int i) const {
-            assert(0<=i&&i<=3);
-            switch(i) {
-                case 0 : return _zi/2;
-                case 1 : return _zi*_zi/2;
-                case 2 : return _zi*(_zi*_zi+1);
-                case 3 : RooComplex _zi2 = _zi*_zi; 
-                         return _zi2*(RooComplex(3,0)+RooComplex(3,0)*_zi2);
-            }
-            assert(1==0);
-            return 0;
-        }
-    private :
-        RooComplex _zi;        
-    };
-
-    class N_n { 
-    public:
-        N_n(double x, RooComplex z) { _n[0] = RooMath::erf(x); _n[1] = exp(-x*x); _n[2] = eval(x,z); }
-        const RooComplex& operator()(int i) const { return _n[i]; }
-    private:
-        RooComplex _n[3];
-    };
 
     class L_jk {
     public:
@@ -139,81 +110,61 @@ namespace {
         double _x;
     };
 
-    class M_n {
-    public:
-       M_n(double x, const RooComplex& z) {
-          L_jk L(x); N_n N(x,z) ;
-          for (int i=0;i<4;++i) _m[i] = N(0)*L(i,0) + N(1)*L(i,1) + N(2)*L(i,2);
-       }
-       const RooComplex& operator()(int i) { assert(0<=i&&i<4); return _m[i]; }
-       M_n& operator-=(const M_n& other) { for(int i=0;i<4;++i) _m[i]= _m[i]-other._m[i]; return *this; }
-       M_n  operator- (const M_n& other) const { return M_n(*this)-=other; }
-    private:
-       RooComplex _m[4];
-    };
 }
+
+RooCubicSplineGaussModel::M_n::M_n(double x, const RooComplex& z) {
+          RooComplex N0( RooMath::erf(x) )
+                   , N1( exp(-x*x)       )
+                   , N2( eval(x,z)       );
+          L_jk L(x); 
+          for (int i=0;i<4;++i) _m[i] = N0*L(i,0) + N1*L(i,1) + N2*L(i,2);
+}
+
 
 //_____________________________________________________________________________
 RooCubicSplineGaussModel::RooCubicSplineGaussModel(const char *name, const char *title
-                , RooRealVar& xIn, const char *knotBinningName, const RooArgList& coefList
-                , RooAbsReal& _mean, RooAbsReal& _sigma) :
-  RooResolutionModel(name,title,xIn), 
+                , RooRealVar& x, RooCubicSplineFun& _spline
+			    , RooAbsReal& _mean, RooAbsReal& _sigma ) :
+  RooResolutionModel(name,title,x), 
   _flatSFInt(kFALSE),
-  splineCoefficients("splineCoeffiients","List of splineCoeffiients",this),
+  spline("spline","Spline describing efficiency",this,_spline),
   mean("mean","Mean",this,_mean),
   sigma("sigma","Width",this,_sigma),
-  msf("msf","Mean Scale Factor",this,RooRealConstant::value(1)),
-  ssf("ssf","Sigma Scale Factor",this,RooRealConstant::value(1)),
-  knots(0)
+  msf("msf","Mean Scale Factor",this,RooRealConstant::value(1.0)),
+  ssf("ssf","Sigma Scale Factor",this,RooRealConstant::value(1.0))
 {  
-  // TODO: verify coefList is consistent with knots as specified by the knotBinningName binning
-  //    should be N+2 coefficients for N bins...
-  const RooAbsBinning* binning = xIn.getBinningPtr(knotBinningName);
-  assert( binning!=0);
-  assert( coefList.getSize()==3+binning->numBins());
- 
-  // Constructor
-  TIterator* coefIter = coefList.createIterator() ;
-  RooAbsArg* coef ;
-  while((coef = (RooAbsArg*)coefIter->Next())) {
-    if (!dynamic_cast<RooAbsReal*>(coef)) {
-      cout << "RooCubicBSpline::ctor(" << GetName() << ") ERROR: coefficient " << coef->GetName() 
-	   << " is not of type RooRealVar" << endl ;
-      assert(0) ;
-    }
-    splineCoefficients.add(*coef) ;
-  }
-  delete coefIter ;
-  Double_t* boundaries = binning->array();
-  knots = new RooCubicSplineKnot( boundaries, boundaries + binning->numBoundaries() );
+    // make sure 'x' matches the spline argument!
+    std::auto_ptr<RooArgSet> svar( spline.arg().getVariables() );
+    assert( svar->contains( convVar() ) );
 }
 
 //_____________________________________________________________________________
 RooCubicSplineGaussModel::RooCubicSplineGaussModel(const char *name, const char *title
-                , RooRealVar& xIn
+                , RooRealVar& x, RooCubicSplineFun& _spline
 			    , RooAbsReal& _mean, RooAbsReal& _sigma
 			    , RooAbsReal& _meanSF, RooAbsReal& _sigmaSF) : 
-  RooResolutionModel(name,title,xIn), 
+  RooResolutionModel(name,title,x), 
   _flatSFInt(kFALSE),
-  splineCoefficients("splineCoeffiients","List of splineCoeffiients",this),
+  spline("spline","Spline describing efficiency",this,_spline),
   mean("mean","Mean",this,_mean),
   sigma("sigma","Width",this,_sigma),
   msf("msf","Mean Scale Factor",this,_meanSF),
-  ssf("ssf","Sigma Scale Factor",this,_sigmaSF),
-  knots(0)
+  ssf("ssf","Sigma Scale Factor",this,_sigmaSF)
 {  
+    // make sure 'x' matches the spline argument!
+    std::auto_ptr<RooArgSet> svar( spline.arg().getVariables() );
+    assert( svar->contains( convVar() ) );
 }
 
 //_____________________________________________________________________________
 RooCubicSplineGaussModel::RooCubicSplineGaussModel(const RooCubicSplineGaussModel& other, const char* name) : 
   RooResolutionModel(other,name),
   _flatSFInt(other._flatSFInt),
-  splineCoefficients("splineCoeffiients",this,other.splineCoefficients),
+  spline("spline",this,other.spline),
   mean("mean",this,other.mean),
   sigma("sigma",this,other.sigma),
   msf("msf",this,other.msf),
-  ssf("ssf",this,other.ssf),
-  knots(new RooCubicSplineKnot(*other.knots) )
+  ssf("ssf",this,other.ssf)
 {
 }
 
@@ -221,7 +172,6 @@ RooCubicSplineGaussModel::RooCubicSplineGaussModel(const RooCubicSplineGaussMode
 RooCubicSplineGaussModel::~RooCubicSplineGaussModel()
 {
   // Destructor
-  delete knots;
 }
 
 //_____________________________________________________________________________
@@ -235,45 +185,37 @@ Int_t RooCubicSplineGaussModel::basisCode(const char* name) const
   return 0 ;
 } 
 
-
-
 //_____________________________________________________________________________
 Double_t RooCubicSplineGaussModel::efficiency() const 
 {
     // TODO: provide a RooAbsReal so we can plot the efficiency -- just like createIntegral...
-    return knots->evaluate(x,splineCoefficients);
+    return spline;
 }
 
 RooComplex RooCubicSplineGaussModel::evalInt(Double_t umin, Double_t umax, const RooComplex& z) const
 {
+    const RooCubicSplineFun &sp = dynamic_cast<const RooCubicSplineFun&>( spline.arg() );
     // TODO: verify we remain within [umin,umax]
-    K_n K(z);
+    RooCubicSplineGaussModel::K_n K(z);
     Double_t scale = sigma*ssf*TMath::Sqrt2(); 
     Double_t offset = mean*msf;
-    std::vector<M_n> M; M.reserve( knots->size() );
-    for (int i=0;i<knots->size();++i) M.push_back( M_n( (knots->u(i)-offset)/scale, z ) );
+    std::vector<M_n> M; M.reserve( sp.knotSize() );
+    for (int i=0;i<sp.knotSize();++i) M.push_back( M_n( (sp.u(i)-offset)/scale, z ) );
     RooComplex sum(0,0);
     double sc[4]; for (int i=0;i<4;++i) sc[i] = pow(scale,i);
-    for (int i=0;i<knots->size()-1;++i) { //TODO: push this loop into RooCubicSplineKnot... pass z,scale,coefficients
-        M_n dM( M[i+1] - M[i] );
-        RooCubicSplineKnot::S_jk S( knots->S_jk_sum( i, splineCoefficients ), offset );
-        for (int j=0;j<4;++j) for (int k=0;k<4-j;++k) {
-            sum = sum + dM(j)*S(j,k)*K(k)*sc[j+k];
-        }
+    for (int i=0;i<sp.knotSize()-1;++i) { //TODO: push this loop into RooCubicSplineFun... pass z,scale,coefficients
+        sum = sum + sp.gaussIntegral( i,  M[i+1] - M[i], K, offset, sc );
     }
     return sum;
 }
-
 
 //_____________________________________________________________________________
 Double_t RooCubicSplineGaussModel::evaluate() const 
 {  
   basisType basisCode = (basisType) _basisCode ;
-
   Double_t tau    = (basisCode!=noBasis)                           ? ((RooAbsReal*)basis().getParameter(1))->getVal() : 0 ;
   Double_t omega  = (basisCode==sinBasis  || basisCode==cosBasis)  ? ((RooAbsReal*)basis().getParameter(2))->getVal() : 0 ;
   Double_t dGamma = (basisCode==sinhBasis || basisCode==coshBasis) ? ((RooAbsReal*)basis().getParameter(2))->getVal() : 0 ;
-
   if (basisCode  == coshBasis && basisCode!=noBasis && dGamma==0 ) basisCode = expBasis;
 
   Double_t scale = sigma*ssf*TMath::Sqrt2();
@@ -351,11 +293,9 @@ Double_t RooCubicSplineGaussModel::analyticalIntegral(Int_t code, const char* ra
   Double_t ssfInt( code==2 ? (ssf.max(rangeName)-ssf.min(rangeName)) : 1.0 );
 
   basisType basisCode = (basisType) _basisCode ;
-
   Double_t tau    = (basisCode!=noBasis)                           ? ((RooAbsReal*)basis().getParameter(1))->getVal() : 0 ;
   Double_t omega  = (basisCode==sinBasis  || basisCode==cosBasis)  ? ((RooAbsReal*)basis().getParameter(2))->getVal() : 0 ;
   Double_t dGamma = (basisCode==sinhBasis || basisCode==coshBasis) ? ((RooAbsReal*)basis().getParameter(2))->getVal() : 0 ;
-
   if (basisCode == coshBasis && basisCode!=noBasis && dGamma==0 ) basisCode = expBasis;
 
   Double_t scale = sigma*ssf*TMath::Sqrt2();
@@ -397,6 +337,5 @@ Double_t RooCubicSplineGaussModel::analyticalIntegral(Int_t code, const char* ra
         assert(0) ;
   }
   if (TMath::IsNaN(result)) { cxcoutE(Tracing) << "RooCubicSplineGaussModel::analyticalIntegral(" << GetName() << ") got nan for basisCode = " << basisCode << endl; }
-
   return scale*result*ssfInt;
 }
