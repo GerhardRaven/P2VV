4d5a87ee4b16b4fb635f9af4892d8a275134c829
Jeroen van Leerdam <jleerdam@nikhef.nl>
2013-06-11 22:55:44 +0200

    always create "noNAN" variable for reading n-tuple

--- python/P2VV/GeneralUtils.py
+++ python/P2VV/GeneralUtils.py
@@ -177,7 +177,7 @@ def readData( filePath, dataSetName, NTuple = False, observables = None, **kwarg
     """reads data from file (RooDataSet or TTree(s))
     """
     from ROOT import RooFit
-    if observables : noNAN = ' && '.join( '( %s==%s )' % ( obs, obs ) for obs in observables )
+    noNAN = ' && '.join( '( %s==%s )' % ( obs, obs ) for obs in observables )
     cuts = kwargs.pop( 'cuts', '' )
     tmp_file = None
     if observables :
