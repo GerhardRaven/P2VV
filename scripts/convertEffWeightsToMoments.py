d9278ab2c212f0dcf2b8697264bed7dfa49f23b1
Jeroen van Leerdam <jleerdam@nikhef.nl>
2014-02-16 12:07:55 +0100

    use combined 2011/2012 dataset in moments scripts

--- scripts/convertEffWeightsToMoments.py
+++ scripts/convertEffWeightsToMoments.py
@@ -1,6 +1,6 @@
 # script parameters
-weightsFile = 'Sim08_20112012_hel_UB_UT_trueTime_BkgCat050_KK30_Phys_norm'
-momentsFile = 'Sim08_20112012_hel_UB_UT_trueTime_BkgCat050_KK30_Phys_moms_norm'
+weightsFile = 'Sim08_hel_UB_UT_trueTime_BkgCat050_KK30_Phys_norm'
+momentsFile = 'Sim08_hel_UB_UT_trueTime_BkgCat050_KK30_Phys_moms_norm'
 funcNames = [ 'Re_ang_A0_A0', 'Re_ang_Apar_Apar', 'Re_ang_Aperp_Aperp'
              , 'Im_ang_Apar_Aperp', 'Re_ang_A0_Apar', 'Im_ang_A0_Aperp'
              , 'Re_ang_AS_AS', 'Re_ang_Apar_AS', 'Im_ang_Aperp_AS', 'Re_ang_A0_AS' ]
