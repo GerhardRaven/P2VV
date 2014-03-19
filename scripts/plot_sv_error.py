ba340c49f302b015bfaded88d08d3ee1ba4b98ec
Roel Aaij <raaij@nikhef.nl>
2014-03-19 16:20:24 +0100

    Write plots to file to be able to plot 2011 and 2012 on the same canvas.

--- scripts/plot_sv_error.py
+++ scripts/plot_sv_error.py
@@ -3,126 +3,53 @@ from P2VV.RooFitWrappers import *
 obj = RooObject( workspace = 'w')
 w = obj.ws()
 
-mass = RealVar( 'B_s0_MM',  Title = 'm(J/#psi K^{+}K^{-})', Unit = 'MeV/c^{2}', Observable = True
-                 , Value = 5368., MinMax = ( 5200., 5550. ),
-                 Ranges = dict(  LeftSideBand  = ( 5200., 5320. )
-                                 , Signal        = ( 5320., 5420. )
-                                 , RightSideBand = ( 5420., 5550. )
-                                 , PeakBkg       = ( 5390., 5440. )
-                                 ))
+mass = RealVar('B_s0_MM', Title = 'm(J/#psi K^{+}K^{-})', Unit = 'MeV/c^{2}', Observable = True,
+               Value = 5368., MinMax = ( 5200., 5550. ))
 st = RealVar('sigmat',Title = '#sigma(t)', Unit = 'ps', Observable = True, MinMax = (0.0, 0.1))
 t  = RealVar('time', Title = 'decay time', Unit='ps', Observable = True, MinMax = (-1.5, 8))
 
 excl_biased = Category('hlt1_excl_biased_dec', Observable = True,
                        States = { 'excl_biased' : 1, 'unbiased' : 0 } )
-prescaled = False
-
-if prescaled:
-    input_file = '/glusterfs/bfys/users/raaij/NTuples/2011/Bs2JpsiPhiPrescaled_ntupleAB_20130531.root'
-    cut = 'sel == 1 && triggerDecisionUnbiasedPrescaled == 1 && '
-else:
-    input_file = '/glusterfs/bfys/users/raaij/NTuples/2012/Bs2JpsiPhi_2012_ntupleAB_20130604.root'
-    cut = '%s==1 && (%s==1 || %s==1) && %s==1 && ' % ('sel', 'hlt1_biased', 'hlt1_unbiased',
-                                                      'hlt2_biased')
-cut += ' && '.join(['%s < 4' % e for e in ['muplus_TRACK_CHI2NDOF', 'muminus_TRACK_CHI2NDOF', 'Kplus_TRACK_CHI2NDOF', 'Kminus_TRACK_CHI2NDOF']])
-cut += ' && sel_cleantail == 1'
-
-for o in [mass, t, st]:
-    cut += ' && {0} > {1} && {0} < {2}'.format(o.GetName(), o.getMin(), o.getMax())
-
-from P2VV.GeneralUtils import readData
-data = readData(filePath = input_file, dataSetName = 'DecayTree', NTuple = True,
-                observables = [mass, t, st, excl_biased], Rename = 'JpsiphiData', ntupleCuts = cut)
-
-sigFrac = 0.504
-nEvents     = data.sumEntries()
-nSignal     = nEvents * sigFrac
-nBackground = nEvents * ( 1. - sigFrac )
-
-from P2VV.Parameterizations.MassPDFs import LP2011_Signal_Mass as SignalBMass
-sig_m = SignalBMass(Name = 'sig_m', mass = mass)
-
-signal = Component( 'signal', [sig_m.pdf()], Yield = ( nSignal,     0., nEvents ) )
-
-from P2VV.Parameterizations.MassPDFs import LP2011_Background_Mass as BackgroundBMass
-bkg_m = BackgroundBMass(Name = 'bkg_m', mass = mass)
-background = Component( 'background', [bkg_m.pdf()], Yield = ( nBackground, 0., nEvents ) )
-
-mass_pdf = buildPdf( [ signal, background ], Observables = [ mass ], Name = 'mass_pdf')
-
-fitOpts = dict(NumCPU = 4, Optimize = 2, Save = True, Timer = True, Minimizer = 'Minuit2',
-               Offset = True)
-
-mass_result = mass_pdf.fitTo(data, **fitOpts)
-
-# categories for splitting the PDF
-if not prescaled:
-    split_cats = [[excl_biased]]
-    # get mass parameters that are split
-    split_params = [[par for par in mass_pdf.Parameters() if par.getAttribute('Yield')]]
-
-    # build simultaneous mass PDF
-    from P2VV.RooFitWrappers import SimultaneousPdf
-    sWeight_mass_pdf = SimultaneousPdf(mass_pdf.GetName() + '_simul'
-                                       , MasterPdf       = mass_pdf
-                                       , SplitCategories = split_cats
-                                       , SplitParameters = split_params)
-
-    # set yields for categories
-    split_cat  = sWeight_mass_pdf.indexCat()
-    split_vars = sWeight_mass_pdf.getVariables()
-    from P2VV.GeneralUtils import getSplitPar
-    from math import sqrt
-    for state in split_cat:
-        sigYield = getSplitPar( 'N_signal', state.GetName(), split_vars )
-        bkgYield = getSplitPar( 'N_background', state.GetName(), split_vars )
-        
-        selStr = '{0} == {0}::{1}'.format(split_cat.GetName(), state.GetName())
-        
-        nEv    = data.sumEntries()
-        nEvBin = data.sumEntries(selStr)
-        
-        sigYield.setVal( sigYield.getVal() * nEvBin / nEv )
-        sigYield.setError( sqrt( sigYield.getVal() ) )
-        sigYield.setMin(0.)
-        sigYield.setMax(nEvBin)
-        bkgYield.setVal( bkgYield.getVal() * nEvBin / nEv )
-        bkgYield.setError( sqrt( bkgYield.getVal() ) )
-        bkgYield.setMin(0.)
-        bkgYield.setMax(nEvBin)
-    
-    # determine mass parameters in each sub-sample with a fit
-    sim_mass_result = sWeight_mass_pdf.fitTo(data, **fitOpts)
-    sweight_pdf = sWeight_mass_pdf
-else:
-    sweight_pdf = mass_pdf
-
-from P2VV.Utilities.SWeights import SData
-sdata = SData( Pdf = sweight_pdf, Data = data, Name = 'mass_sdata')
-sig_sdata = sdata.data('signal')
-bkg_sdata = sdata.data('background')
 
 from ROOT import TFile
-f = TFile.Open(input_file, "update")
-tree = f.Get("DecayTree")
-
-from ROOT import addVertexErrors
-from ROOT import std
-dss = std.list("RooDataSet*")()
-dss.push_back(sig_sdata)
-dss.push_back(bkg_sdata)
-addVertexErrors(tree, dss, cut)
+input_file = 'vx_errors.root'
+f = TFile.Open(input_file)
+datas = {}
+for d in ['2011', '2012']:
+    datas[d] = f.Get(d + '/' + 'sig_sdata')
 
 from P2VV.Load import LHCbStyle
 
 from ROOT import TCanvas
+__canvs = []
+from ROOT import kOrange
+from ROOT import kBlue
+
+from collections import defaultdict
+frames = {}
+
 canvases = [TCanvas(n, n, 600, 400) for n in ['pv_canvas', 'sv_canvas', 'st_canvas', 'psi_canvas']]
-for canvas, (vn, xt) in zip(canvases, [('pv_err', '#sigma_{t,PV} [ps]'), ('sv_err', '#sigma_{t,SV} [ps]'),
-                                        (st.GetName(), '#sigma_{t} [ps]'), ('jpsi_vx_err', '#sigma_{t,J/#psi} [ps]')]):
+for color, (d, data) in zip((kOrange, kBlue), datas.items()):
+    for canvas, (vn, xt) in zip(canvases, [('pv_err', '#sigma_{t,PV} [ps]'), ('sv_err', '#sigma_{t,SV} [ps]'),
+                   (st.GetName(), '#sigma_{t} [ps]'), ('jpsi_vx_err', '#sigma_{t,J/#psi} [ps]')]):
+        sw = data.sumEntries()
+        if vn not in frames:
+            var = data.get().find(vn)
+            frame = var.frame()
+            frame.GetXaxis().SetTitle(xt)
+            frame.GetYaxis().SetTitle('a.u.')
+            frame.GetYaxis().SetTitleOffset(1)
+            frames[vn] = canvas, frame
+        else:
+            frame = frames[vn][1]
+        data.plotOn(frame, MarkerColor = color, Rescale = 1 / sw)
+
+for canvas, frame in frames.itervalues():
     canvas.cd()
-    var = sig_sdata.get().find(vn)
-    frame = var.frame()
-    frame.GetXaxis().SetTitle(xt)
-    frame.GetYaxis().SetTitle('Candidates / fs')
-    sig_sdata.plotOn(frame)
+    graph = frame.getObject(0)
+    y = graph.GetY()
+    n = graph.GetN()
+    m = max(y[i] for i in range(n))
+    frame.SetMaximum(1.1 * m)
     frame.Draw()
+
