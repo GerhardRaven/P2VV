edaa10f89feda212575d87a2f31e8fe088434375
Jeroen van Leerdam <jleerdam@nikhef.nl>
2014-03-30 20:39:00 +0200

    added class to analyse fit results

--- python/P2VV/Utilities/Studies.py
+++ python/P2VV/Utilities/Studies.py
@@ -164,3 +164,99 @@ class parameterGen(object) :
         for name, val, err in self.stats() :
             prec = max( 0, 3 - int( ceil( log10(err) ) ) )
             print ( '  {0:<%ds}  {1:<+10.%df} +/- {2:<10.%df}' % ( nameLen, prec, prec ) ).format( name, val, err )
+
+# class to analyse fit results
+class fitResultsAnalysis(object) :
+    def __init__( self, ParNames, AnaFiles = [ ], RefFile = '' ) :
+        assert ParNames and hasattr( ParNames, '__iter__' ) and hasattr( ParNames, '__len__' ) and len(ParNames) > 0, 'P2VV'\
+               'P2VV - ERROR: fitResultsAnalysis: no parameter names specified'
+        self._parNames = ParNames
+        from P2VV.Parameterizations.FullPDFs import PdfConfiguration
+        self._refPars = PdfConfiguration()
+        self._anaPars = PdfConfiguration()
+        self._refParVals = [ None ] * len(self._parNames)
+        self._anaParVals = [ ]
+        self.readRefFile(RefFile)
+        self.readAnaFiles(AnaFiles)
+
+    def readRefFile( self, file ) :
+        self._refParVals = [ None ] * len(self._parNames)
+        if file :
+            self._refPars.readParametersFromFile( filePath = file )
+            parVals = self._refPars.parameters()
+            for parIt, name in enumerate(self._parNames) :
+                assert name in parVals\
+                       , 'P2VV - ERROR: fitResultsAnalysis.readRefFile(): parameter "%s" not found in file "%s"' % ( name, file )
+                self._refParVals[parIt] = ( parVals[name][0], parVals[name][1] )
+
+    def readAnaFiles( self, files ) :
+        self._anaParVals = [ ]
+        if files :
+            fileStats = { }
+            for file in files :
+                fitStatus = self._anaPars.readParametersFromFile( filePath = file, Verbose = False )
+                if fitStatus[0] :
+                    print 'P2VV - WARNING: fitResultsAnalysis.readAnaFiles(): fit status %d for file "%s"' % ( fitStatus[0], file )
+                if not fitStatus[0] in fileStats :
+                    fileStats[ fitStatus[0] ] = 1
+                else :
+                    fileStats[ fitStatus[0] ] += 1
+
+                self._anaParVals.append( [ ] )
+                parVals = self._anaPars.parameters()
+                for name in self._parNames :
+                    assert name in parVals\
+                           , 'P2VV - ERROR: fitResultsAnalysis.readAnaFiles(): parameter "%s" not found in file "%s"' % ( name, file )
+                    self._anaParVals[-1].append( parVals[name][0] )
+
+            self._nFiles = sum( stats for stats in fileStats.itervalues() )
+            print 'P2VV - INFO: fitResultsAnalysis.readAnaFiles(): read %d parameter files for analysis (fit status: %s)'\
+                  % ( self._nFiles, ', '.join( '%d: %d' % ( stat, count ) for stat, count in fileStats.iteritems() ) )
+
+    def processResults( self, histsFile = '' ) :
+        if not self._anaParVals :
+            print 'P2VV - WARNING: fitResultsAnalysis.processResults(): no parameter values available for analysis'
+            return
+
+        self._parSums = [ [ 0., 0., 0., 0. ] for par in self._parNames ]
+        self._nParVals = [ [ 0., 0., 0. ] for par in self._parNames ]
+        for vals in self._anaParVals :
+            for valIt, val in enumerate(vals) :
+                self._parSums[valIt][0] += val
+                self._parSums[valIt][1] += val**2
+                self._nParVals[valIt][0] += 1
+                if self._refParVals[valIt] != None :
+                    valDiff = val - self._refParVals[valIt][0]
+                    self._parSums[valIt][ 3 if valDiff < 0. else 2 ] += valDiff**2
+                    self._nParVals[valIt][ 2 if valDiff < 0. else 1 ] += 1
+        for valCounts in self._nParVals :
+            assert valCounts[0] == self._nFiles and ( valCounts[1] + valCounts[2] == self._nFiles or self._refParVals[0] == None )
+
+        from math import sqrt, log10, ceil
+        nameLen = min( 30, max( len(name) for name in self._parNames ) )
+        sepStr = '  ' + '-' * ( nameLen + ( 97 if self._refParVals[0] != None else 31 ) )
+        print 'P2VV - INFO: fitResultsAnalysis.processResults(): parameter statistics for %d files:' % self._nFiles
+        print sepStr
+        print ( '  {0:<%ds}   {1:<8s}   {2:<11s}' % nameLen ).format( 'name', 'mean', 'std. dev.' ),
+        if self._refParVals[0] != None :
+            print '   {0:<8s}   {1:<7s}   {2:<11s}   {3:<10s}   {4:<11s}   {5:<11s}'\
+                  .format( 'value', 'uncert.', 'mean dev.', 'dev. (sig)', '+dev. (sig)', '-dev. (sig)' )
+        else :
+            print
+        print sepStr
+        for name, parSums, nParVals, refVal in zip( self._parNames, self._parSums, self._nParVals, self._refParVals ) :
+            meanVal = parSums[0] / float( nParVals[0] )
+            meanSqVal = parSums[1] / float( nParVals[0] )
+            stdDev = sqrt( meanSqVal - meanVal**2 )
+            precDev = max( 0, 3 - int( ceil( log10(stdDev) ) ) )
+            prec = max( 0, 2 - int( ceil( log10( refVal[1] ) ) ) ) if refVal != None else precDev
+            print ( '  {0:<%ds}   {1:<+8.%df}   {2:<11.%df}' % ( nameLen, prec, precDev ) ).format( name, meanVal, stdDev ),
+            if refVal != None :
+                dev = sqrt( ( parSums[2] + parSums[3] ) / float( nParVals[0] ) ) / refVal[1]
+                devPlus = sqrt( parSums[2] / float( nParVals[1] ) ) / refVal[1]
+                devMin  = sqrt( parSums[3] / float( nParVals[2] ) ) / refVal[1]
+                print ( '   {0:<+8.%df}   {1:<7.%df}   {2:<+11.%df}   {3:<10.3f}   {4:<11.3f}   {5:<11.3f}' % ( prec, prec, precDev ) )\
+                      .format( refVal[0], refVal[1], meanVal - refVal[0], dev, devPlus, devMin )
+            else :
+                print
+        print sepStr
