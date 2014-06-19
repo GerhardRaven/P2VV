8797477d950acd0b48d72b12cdca3f9e62e897fd
Jeroen van Leerdam <jleerdam@nikhef.nl>
2014-06-19 11:05:45 +0200

    implemented conversion of multiple datasets in one file in scripts/convertDataSet.py

--- scripts/convertDataSet.py
+++ scripts/convertDataSet.py
@@ -1,22 +1,25 @@
-dataName = 'JpsiKK_sigSWeight'
+dataNames = [ 'JpsiKK', 'JpsiKK_sigSWeight', 'JpsiKK_cbkgSWeight' ]
 cut = 'hlt2_biased==1'
 removeObs = [ 'wMC', 'mdau1', 'tagCatP2VV' ] #, 'polarity', 'hlt2_prescale', 'nPVCat', 'BpTCat' ]
-dataFilePathIn  = 'P2VVDataSets20112012Reco14_LbLifetime_I2Mass_6KKMassBins_2TagCats.root'
-dataFilePathOut = 'P2VVDataSets20112012Reco14_LbLifetime_I2Mass_6KKMassBins_2TagCats_HLT2B.root'
-
-from ROOT import TFile
-dataFile = TFile.Open(dataFilePathIn)
-data = dataFile.Get(dataName)
-print 'read dataset from file "%s"' % dataFile.GetName()
-data.Print()
+dataFilePathIn  = 'P2VVDataSets20112012Reco14_I2Mass_6KKMassBins_4TagCats.root'
+dataFilePathOut = 'P2VVDataSets20112012Reco14_I2Mass_6KKMassBins_4TagCats_HLT2B.root'
 
 import P2VV.RooFitWrappers
-from ROOT import TObject, RooFit, RooDataSet, RooArgSet
-newArgSet = RooArgSet( data.get() )
-for name in removeObs : newArgSet.remove( newArgSet.find(name) )
-newData = RooDataSet( dataName, dataName, newArgSet, RooFit.Import(data), RooFit.Cut(cut) )
-newData.Print()
+from ROOT import TObject, TFile, RooFit, RooDataSet, RooArgSet
+dataFile = TFile.Open(dataFilePathIn)
 newDataFile = TFile.Open( dataFilePathOut, 'RECREATE' )
-newDataFile.Add(newData)
+newData = [ ]
+print 'read datasets from file "%s"' % dataFile.GetName()
+for dataName in dataNames :
+    print 'reading dataset "%s"' % dataName
+    data = dataFile.Get(dataName)
+    data.Print()
+
+    newArgSet = RooArgSet( data.get() )
+    for name in removeObs : newArgSet.remove( newArgSet.find(name) )
+    newData.append( RooDataSet( dataName, dataName, newArgSet, RooFit.Import(data), RooFit.Cut(cut) ) )
+    newData[-1].Print()
+    newDataFile.Add( newData[-1] )
+
 print 'write dataset to file "%s"' % newDataFile.GetName()
 newDataFile.Write( dataFilePathOut, TObject.kOverwrite )
