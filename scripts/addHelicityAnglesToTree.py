d5492df48acf9b00b6f6bebad87e83ac670bda4c
vsyropou <vsyropou@nikhef.nl>
2014-06-24 16:56:44 +0200

    In ../scripts/addHelicityAnglesToTree.py: Added command line arguments.

--- scripts/addHelicityAnglesToTree.py
+++ scripts/addHelicityAnglesToTree.py
@@ -8,7 +8,7 @@ options = parser.parse_args()
 #######################################################
 ## configure ##
 #######################################################
-dataSetPath = '/data/bfys/vsyropou/Bs2JpsiKst/DiegosUnrefinedTuples/2011n.root'
+dataSetPath = '/data/bfys/vsyropou/Bs2JpsiKst/DiegosUnrefinedTuples/2011p.root'
 dataSetName = 'DecayTree'
 
 daughterPartNames  = dict( posHad='Kplus'    if not options.flipHadrons else 'piminus',  
@@ -21,7 +21,7 @@ caseSpecifier = '_%s_%s_%s_%s'%( daughterPartNames['posHad'][:3],daughterPartNam
                                  daughterPartNames['posLep'][:3],daughterPartNames['negLep'][:3] )
 
 # calcualted angle names
-angleNames = {}
+angleNames, oldangleNames = {},{}
 angleNames['helcosthetaK'] = 'helcosthetaK_%s' %caseSpecifier
 angleNames['helcosthetaL'] = 'helcosthetaL_%s'%caseSpecifier
 angleNames['helphi']       = 'helphi_%s'%caseSpecifier
@@ -35,6 +35,7 @@ oldangleNames['helphi']       = 'B0_Phi'
 ####################################
 
 # open input file
+from ROOT import TFile
 f = TFile.Open(dataSetPath)
 t = f.Get(dataSetName)
 t.SetBranchStatus('*',0)
@@ -53,7 +54,7 @@ del f
 
 # masses
 from P2VV.Load import P2VVLibrary
-from ROOT import TDatabasePDG, TFile, addHelicityAnglesToTree, TCanvas, TH1D
+from ROOT import TDatabasePDG, addHelicityAnglesToTree
 from math import pi
 
 MeV = 1000 # TDatabasePDG is in GeV
@@ -94,6 +95,7 @@ print 'P2VV - INFO: Wrote file: %s'%dataSetPath[-10:-5] + '_%s.root'%caseSpecifi
 file_ = TFile.Open(dataSetPath[-10:-5] + '_%s.root'%caseSpecifier)
 tree = file_.Get('DecayTree')
 
+from ROOT import TH1D
 h_cthK = TH1D('cthK','cthK',100,-1,1)
 h_cthL = TH1D('cthL','cthL',100,-1,1)
 h_phi  = TH1D('phi','phi',100,-pi-.5,pi+.5)
@@ -104,7 +106,7 @@ h_my_phi  = TH1D('myphi','myphi',100,-pi-.5,pi+.5)
 
 h_pull_cthK = TH1D('cThKpull','cThKpull', 100, -1e-6, 1e-6)
 h_pull_cthL = TH1D('cThLpull','cThLpull', 100, -1e-6, 1e-6)
-h_pull_phi  = TH1D('phipull','phipull', 100, -5e-6, 5e-6)
+h_pull_phi  = TH1D('phipull','phipull',   100, -1e-6, 1e-6)
 
 # new comparision
 for entry in tree:
@@ -122,6 +124,7 @@ for entry in tree:
     
 # compare
 if options.compare:
+    from ROOT import TCanvas
     c = TCanvas('calculated angles','calculated angles')
     c.Divide(3,2)
     
@@ -156,3 +159,6 @@ if options.compare:
     
     c2.cd(3)
     h_pull_phi.Draw()
+
+    c.Print('angles_%s%s.pdf'%(dataSetPath[-10:-5],caseSpecifier))
+    c2.Print('angles_pulls_%s%s.pdf'%(dataSetPath[-10:-5],caseSpecifier))
