0c61a6b29df01003d8725fd5eb25898528bbe202
vsyropou <vsyropou@nikhef.nl>
2014-07-01 20:53:08 +0200

    In scripts/addHelicityAnglesToTree.py: Improve comparision scatter plots.

--- scripts/addHelicityAnglesToTree.py
+++ scripts/addHelicityAnglesToTree.py
@@ -5,7 +5,8 @@ parser.add_argument( '-L', '--flipLeptons',      default = False,  action = 'sto
 parser.add_argument( '-c', '--compare',          default = False,  action = 'store_true' )
 parser.add_argument( '-m', '--minimalTuples',    default = False,  action = 'store_true' )
 parser.add_argument( '-a', '--addAllConfigrtns', default = False,  action = 'store_true' )
-parser.add_argument( '-f', '--KstFlavorCut',     default = ''                            )
+parser.add_argument( '-r', '--reduced',          default = False,  action = 'store_true' )
+parser.add_argument( '-f', '--ManualKstFlavor',  default = ''                            )
 options = parser.parse_args()
 if options.addAllConfigrtns:
     options.flipHadrons = False
@@ -15,20 +16,20 @@ if options.addAllConfigrtns:
 ## configure ##
 #######################################################
 from P2VV.Load import P2VVLibrary
-dataSetPath = '/home/vilain/Desktop/workDir/data/storage/data/Bd2JpsiKstar_DTT_after_yuehongs_script_20120203.root'
-# '~/Desktop/workDir/data/storage/data/2011p.root' # '~/Desktop/workDir/data/storage/data/Bd2JpsiKstar_DTT_after_yuehongs_script_20120203.root'
-dataSetName = 'DecayTree'
+dataSetPath   = '/data/bfys/vsyropou/Bs2JpsiKst/edinburgTuples/' 
+dataSetFile   = 'Bd2JpsiKstar_DTT_after_yuehongs_script_20120203.root' #'Bd2JpsiKstar_biased_Selected_20120202_sw_eff.root'   # 
+dataSetName   = 'DecayTree'
+minimalNtuple = options.minimalTuples
 
 daughterPartNames  = dict( posHad='Kplus'    if not options.flipHadrons else 'piminus',  
                            negHad='piminus'  if not options.flipHadrons else 'Kplus',  
                            posLep='muplus'   if not options.flipLeptons else 'muminus',  
                            negLep='muminus'  if not options.flipLeptons else 'muplus'
                           )
-
 caseSpecifier = '_%s_%s_%s_%s'%( daughterPartNames['posHad'][:3],daughterPartNames['negHad'][:3],\
                                  daughterPartNames['posLep'][:3],daughterPartNames['negLep'][:3] )
 
-# calcualted angle names
+# angle names
 angleNames, oldangleNames = {},{}
 angleNames['helcosthetaK'] = 'helcosthetaK_%s' %caseSpecifier
 angleNames['helcosthetaL'] = 'helcosthetaL_%s'%caseSpecifier
@@ -38,15 +39,18 @@ oldangleNames['helcosthetaK'] = 'B0_ThetaK'
 oldangleNames['helcosthetaL'] = 'B0_ThetaL'
 oldangleNames['helphi']       = 'B0_Phi'
 
-minimalNtuple = options.minimalTuples
-if options.KstFlavorCut=='neg':
+# manually select Kst flavour, if datasets are not already splited.
+if options.ManualKstFlavor=='neg':
     print 'P2VV - INFO: Selecting negative Kaons only.'
     selectionString = 'Kplus_ID == -321' 
-elif options.KstFlavorCut=='pos':
+elif options.ManualKstFlavor=='pos':
     selectionString = 'Kplus_ID == 321' 
     print 'P2VV - INFO: Selecting positive Kaons only.'
-else: selectionString = ''
-# else: assert False, 'P2VV - INFO: Provide valid argument (-f) for Kaon sign {pos/neg}.'
+else: 
+    selectionString = ''
+    print 'P2VV - INFO: Assuming that dataset is already split for pos/neg Kaon. No cut applied.'
+if   options.reduced and selectionString:     selectionString += ' && runNumber < 92e3'
+elif options.reduced and not selectionString: selectionString += 'runNumber < 92e3'
 
 ##################################
 ## calculate helicity angles ##
@@ -54,11 +58,11 @@ else: selectionString = ''
 
 # open input file
 from ROOT import TFile
-f = TFile.Open(dataSetPath)
+f = TFile.Open(dataSetPath + dataSetFile)
 t = f.Get(dataSetName)
 
 # create intermediate file
-tempFile = TFile.Open(dataSetPath[-10:-5] + '_%s.root'%caseSpecifier,'recreate')
+tempFile = TFile.Open(dataSetFile[:-5] + '_%s.root'%caseSpecifier,'recreate')
 if selectionString:
     print 'P2VV - INFO: Applying the following cuts %s. Initial entries: %s'%(selectionString,t.GetEntries())
     tree = t.CopyTree(selectionString)
@@ -129,7 +133,7 @@ if options.addAllConfigrtns:
     print 'P2VV - INFO: Fliping hadrons and leptons and recalculating helicity angles'
     addHelicityAnglesToTree(tree, 
                             daughterPartNames['negHad'], daughterPartNames['posHad'], 
-                            daughterPartNames['negLep'], daughterPartNames['posLep'], 
+                            daughterPartNames['negLep'], daughterPartNames['posLep'],
                             negHadMass, posHadMass, lepMass, lepMass,
                             'helcosthetaK__pim_Kpl_mum_mup','helcosthetaL__pim_Kpl_mum_mup','helphi__pim_Kpl_mum_mup',
                             'Float_t')
@@ -140,15 +144,15 @@ tree.Write()
 tree.Show()
 tempFile.Close()
 del tempFile, tree
-print 'P2VV - INFO: Wrote file: %s'%dataSetPath[-10:-5] + '_%s.root'%caseSpecifier
+print 'P2VV - INFO: Wrote file: %s'%dataSetFile[:-5] + '_%s.root'%caseSpecifier
 
 if options.compare:
     # re-open outfile
-    file_ = TFile.Open(dataSetPath[-10:-5] + '_%s.root'%caseSpecifier)
+    file_ = TFile.Open(dataSetFile[:-5] + '_%s.root'%caseSpecifier)
     tree = file_.Get('DecayTree')
     
     from math import cos 
-    from ROOT import TH1D, TH2D
+    from ROOT import TH1D, TH2D, TCanvas
 
     h_cthK = TH1D('cthK','cthK',100,-1,1)
     h_cthL = TH1D('cthL','cthL',100,-1,1)
@@ -157,14 +161,10 @@ if options.compare:
     h_my_cthK = TH1D('mycthK','mycthK',100,-1,1)
     h_my_cthL = TH1D('mycthL','mycthL',100,-1,1)
     h_my_phi  = TH1D('myphi','myphi',100,-pi-.5,pi+.5)
-    
-    # h_pull_cthK = TH1D('cThKpull','cThKpull', 100, -1e-6, 1e-6)
-    # h_pull_cthL = TH1D('cThLpull','cThLpull', 100, -1e-6, 1e-6)
-    # h_pull_phi  = TH1D('phipull','phipull',   100, -1e-6, 1e-6)
 
-    h_scat_cthK = TH2D('cThKscat','cThKscat', 100, -1, 1,100, -1, 1)
-    h_scat_cthL = TH2D('cThLscat','cThLscat', 100, -1, 1,100, -1, 1)
-    h_scat_phi  = TH2D('phiscat','phiscat',   100, -pi, pi, 100, -pi, pi)
+    h_scat_cthK = TH2D('helcosthetaK', 'helcosthetaK', 100, -1, 1,   100, -1, 1)
+    h_scat_cthL = TH2D('helcosthetaL', 'helcosthetaL', 100, -1, 1,   100, -1, 1)
+    h_scat_phi  = TH2D('helphi',       'helphi',       100, -pi, pi, 100, -pi, pi)
 
     # new comparision
     for entry in tree:
@@ -176,72 +176,47 @@ if options.compare:
         h_cthL.Fill(cos(getattr(entry,oldangleNames['helcosthetaL'])))
         h_phi.Fill(getattr(entry,oldangleNames['helphi']))
 
-        h_scat_cthK.Fill(getattr(entry,angleNames['helcosthetaK']),   cos(getattr(entry,oldangleNames['helcosthetaK'])))
-        h_scat_cthL.Fill(getattr(entry,angleNames['helcosthetaL']),   cos(getattr(entry,oldangleNames['helcosthetaL'])))
-        h_scat_phi. Fill(getattr(entry,angleNames['helphi']      ),       getattr(entry,oldangleNames['helphi']       ))
+        h_scat_cthK.Fill( getattr(entry,angleNames['helcosthetaK']),   cos(getattr(entry,oldangleNames['helcosthetaK'])) )
+        h_scat_cthL.Fill( getattr(entry,angleNames['helcosthetaL']),   cos(getattr(entry,oldangleNames['helcosthetaL'])) )
+        h_scat_phi. Fill( getattr(entry,angleNames['helphi']      ),       getattr(entry,oldangleNames['helphi']       ) )
         
         # if getattr(entry,oldangleNames['helphi']) < 0: 
         #            h_scat_phi. Fill(angleNames['helphi'], getattr(entry,oldangleNames['helphi']) - pi)
         # else: h_scat_phi. Fill(angleNames['helphi'], getattr(entry,oldangleNames['helphi']) + pi)
         
-
-           
-        # h_cthK.Fill(getattr(entry,oldangleNames['helcosthetaK'])) 
-        # h_cthL.Fill(getattr(entry,oldangleNames['helcosthetaL']))
-        # h_phi.Fill(getattr(entry,oldangleNames['helphi']))
-        
-        # h_pull_cthK.Fill(getattr(entry,angleNames['helcosthetaK'])    - getattr(entry,oldangleNames['helcosthetaK']) )
-        # h_pull_cthL.Fill(getattr(entry,angleNames['helcosthetaL'])    - getattr(entry,oldangleNames['helcosthetaL']) )
-        # h_pull_phi.Fill (getattr(entry,angleNames['helphi'])          - getattr(entry,oldangleNames['helphi']) )
-        
-# compare
-if options.compare:
-    from ROOT import TCanvas
+     
     c = TCanvas('calculated angles','calculated angles')
     c.Divide(3,2)
-    
     c.cd(1)
-    h_cthK.Draw()
-    
+    h_cthK.Draw()    
     c.cd(2)
     h_cthL.Draw()
-    
     c.cd(3)
     h_phi.Draw()
-    
     c.cd(4)
     h_my_cthK.Draw()
-    
     c.cd(5)
     h_my_cthL.Draw()
-    
     c.cd(6)
     h_my_phi.Draw()
     
     # scater plots
-    c3 = TCanvas('scatter','scatter')
-    c3.Divide(2,2)
-    c3.cd(1)
-    tree.Draw('%s:%s'%(oldangleNames['helcosthetaK'],angleNames['helcosthetaK']))
-    c3.cd(2)
-    tree.Draw('%s:%s'%(oldangleNames['helcosthetaL'],angleNames['helcosthetaL']))    
-    c3.cd(3)
-    tree.Draw('%s:%s'%(oldangleNames['helphi'],angleNames['helphi']))
-    
-    
-    # pulls
-    c2 = TCanvas('pulls','pulls')
-    c2.Divide(2,2)
-    
-    c2.cd(1)
-    h_pull_cthK.Draw()
-    
-    c2.cd(2)
-    h_pull_cthL.Draw()
-    
-    c2.cd(3)
-    h_pull_phi.Draw()
+    for hist in [h_scat_cthK,h_scat_cthL,h_scat_phi]:
+        hist.SetStats(0)
+        hist.SetXTitle('Edinburg')
+        hist.SetYTitle('Vasilis')
 
-    c.Print('angles_%s%s.pdf'%(dataSetPath[-10:-5],caseSpecifier))
-    c2.Print('angles_pulls_%s%s.pdf'%(dataSetPath[-10:-5],caseSpecifier))
-    c3.Print('angles_scatters_%s%s.pdf'%(dataSetPath[-10:-5],caseSpecifier))
+    c3 = TCanvas('scatter','scatter')
+    c3.Divide(3,1)
+    c3.cd(1)
+    h_scat_cthK.Draw()
+    # tree.Draw('TMath::Cos(%s):%s'%(oldangleNames['helcosthetaK'],angleNames['helcosthetaK']))
+    c3.cd(2)
+    h_scat_cthL.Draw()
+    # tree.Draw('TMath::Cos(%s):%s'%(oldangleNames['helcosthetaL'],angleNames['helcosthetaL']))    
+    c3.cd(3)
+    h_scat_phi.Draw()
+    #tree.Draw('%s:%s'%(oldangleNames['helphi'],angleNames['helphi']))
+    
+    c.Print('angles_%s%s.pdf'%(dataSetFile[:-5],caseSpecifier))
+    c3.Print('angles_scatters_%s%s.pdf'%(dataSetFile[:-5],caseSpecifier))
