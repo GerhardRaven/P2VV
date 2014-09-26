aece6b89e1ae827d9f5dff3662dd38e42878d7b6
Jeroen van Leerdam <jleerdam@nikhef.nl>
2014-09-26 11:31:08 +0200

    added option to split dataset into run periods to scripts/convertDataSet.py

--- scripts/convertDataSet.py
+++ scripts/convertDataSet.py
@@ -1,11 +1,13 @@
-dataNames = [ 'JpsiKK', 'JpsiKK_sigSWeight', 'JpsiKK_cbkgSWeight' ]
+runPeriod = 2012
 cut = 'hlt2_biased==1'
+if runPeriod : cut += ( ' && ' if cut else '' ) + 'runPeriod==%d' % runPeriod
+dataNames = [ 'JpsiKK_sigSWeight' ] #[ 'JpsiKK', 'JpsiKK_sigSWeight', 'JpsiKK_cbkgSWeight' ]
 removeObs = [ 'wMC', 'mdau1', 'tagCatP2VV' ] #, 'polarity', 'hlt2_prescale', 'nPVCat', 'BpTCat' ]
-dataFilePathIn  = 'P2VVDataSets20112012Reco14_I2Mass_6KKMassBins_4TagCats.root'
-dataFilePathOut = 'P2VVDataSets20112012Reco14_I2Mass_6KKMassBins_4TagCats_HLT2B.root'
+dataFilePathIn  = 'P2VVDataSets20112012Reco14_I2Mass_6KKMassBins_2TagCats_20140309.root'
+dataFilePathOut = 'P2VVDataSets2012Reco14_I2Mass_6KKMassBins_2TagCats_20140309.root'
 
 import P2VV.RooFitWrappers
-from ROOT import TObject, TFile, RooFit, RooDataSet, RooArgSet
+from ROOT import TObject, TFile, RooFit, RooDataSet, RooArgSet, RooCategory
 dataFile = TFile.Open(dataFilePathIn)
 newDataFile = TFile.Open( dataFilePathOut, 'RECREATE' )
 newData = [ ]
@@ -17,6 +19,12 @@ for dataName in dataNames :
 
     newArgSet = RooArgSet( data.get() )
     for name in removeObs : newArgSet.remove( newArgSet.find(name) )
+    if runPeriod :
+        newArgSet.remove( newArgSet.find('runPeriod') )
+        rp = RooCategory( 'runPeriod', 'runPeriod' )
+        rp.defineType( 'p%d' % runPeriod, runPeriod )
+        newArgSet.add(rp)
+
     newData.append( RooDataSet( dataName, dataName, newArgSet, RooFit.Import(data), RooFit.Cut(cut) ) )
     newData[-1].Print()
     newDataFile.Add( newData[-1] )
